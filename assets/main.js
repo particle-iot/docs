7f5a4bb38fc1673ed21a4c926411b0a51d69ca6a
Zach Supalla <zsupalla@gmail.com>
2013-11-20 12:16:32 -0600

    Added javascript routing

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,20 @@
+var app = Sammy('#docs', function() {
+
+  this.get('#/', function() {
+    console.log('home!');
+  })
+
+  this.get('#/:section', function() {
+    Flatdoc.run({
+      fetcher: Flatdoc.file('docs/' + this.params['section'] + '.md')
+    });
+  });
+});
+
 (function ($) {
   $(document).ready(
     function(){
-      $('.header ul a').on('click', function(event) {
-        event.preventDefault();
-        var href = $(this).attr('href');
-        href = href.replace('#', '');
-        Flatdoc.run({
-          fetcher: Flatdoc.file('docs/' + href + '.md')
-        });
-      })
+      app.run('#/');
     }
   )
 })(window.jQuery)
