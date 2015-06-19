b17ac9dec43cb054b468c654e519d4bb16f577d3
Zach Supalla <zsupalla@gmail.com>
2015-06-18 23:54:44 -0700

    WIP: Semi-broken sidenav in progress

--- src/assets/js/docs.js
+++ src/assets/js/docs.js
@@ -70,9 +70,55 @@ Derived from Flatdoc (http://ricostacruz.com/flatdoc)
     }
   };
 
+  Docs.buildTableOfContents = function() {
+
+    var pathArray = window.location.pathname.split('/');
+    var page = pathArray[pathArray.length - 2];
+
+    // var $toc = $('.menubar');
+    var $toc = $('.menubar a[href*="' + page + '"]');
+    console.log($toc);
+
+    // create menu object
+    var menu = [];
+
+    $('.content').find('h1, h2, h3').each(function() {
+      var $el = $(this);
+      var level = +($el[0].tagName.substr(1));
+
+      var obj = { section: $el.text(), items: [], level: level, id: $el.attr('id') };
+      menu.push(obj);
+    });
+
+
+    // Add the TOC
+    if (menu.length > 0) {
+      menu.forEach(function(item) {
+        var id = item.id;
+
+        var $li = $('<li>')
+          .attr('id', id + '-item')
+          .addClass('level-' + item.level);
+
+        if (item.section) {
+          var $a = $('<a>')
+            .html(item.section)
+            .attr('id', id + '-link')
+            .attr('href', '#' + id)
+            .addClass('level-' + item.level);
+            $li.append($a);
+        }
+
+        $toc.append($li);
+
+      });
+    }
+  };
+
   // Ok, then let's do it!
   Docs.rememberDevices();
   Docs.transform();
+  Docs.buildTableOfContents();
   prettyPrint();
 
 })(jQuery);
