211c4e78c6ca24f89bbd7516edcfcaf27db35f4b
Jeff Eiden <jeff.m.eiden@gmail.com>
2015-06-28 21:04:34 -0700

    added RequireJS for browser JS modules, and started the process of saving progress. Still a long way to go

---
+++ src/assets/js/modules/app.js
@@ -0,0 +1,12 @@
+requirejs.config({
+  paths: {
+    jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+    lodash: "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.9.3/lodash"
+  }
+});
+// Start the main app logic.
+requirejs(['utils/storage', 'core/progress', 'core/state', 'jquery', 'lodash'],
+function   (storage, progress, state, $, _) {
+  console.log(_);
+  progress.mark();
+});
