ff21e705e70b81d9ae4ebf6ac9242ac5417cd4d1
Corey Hobbs <chobberoni@gmail.com>
2015-11-03 01:12:04 -0800

    Form is Working | Stage 2
    Validation scripts added.
    Went with AJAX for ease of use and clarity.
    Modularity of functions coming later.
    Styling of alerts coming later.
    Heroic REST client also being pushed on different REPO.

---
+++ src/assets/js/checkform.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+	
+	$("#form1").on("submit", function(e) {
+		e.preventDefault();
+
+		console.log("Handling the submit");
+		//add error handling here
+		//gather the form data
+
+		var datax = {};
+		datax.name = $("#name").val();
+		//data.cat = $("#category").val();
+		datax.email = $("#email").val();
+		datax.email2 = $("#email2").val();
+		datax.subj = $("#category option:selected").val();
+		datax.comments = $("#comment").val();
+
+
+		//Validations
+		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
+		if( !regex.test(datax.email)) { alert('enter a valid email'); }
+
+    if (datax.email != datax.email2)
+    {
+      alert('Those emails don\'t match!');
+      return false;
+    } 
+
+    if (datax.name == '') {
+    	swal("Here's a message!")
+    	return false;
+    }
+
+    if (datax.comments == ''){
+    	alert('Please leave a comment');
+    	return false;
+    }
+
+
+		//Post to Heroku App
+		$.ajax({
+		  method: "POST",
+		  url: "http://localhost:8081",
+		  data: { name: datax.name, 
+		  				email: datax.email ,
+		  				subject: datax.subj,
+		  				comments: datax.comments
+						}
+		})//post function
+		  .done(function( msg ) {
+		    alert( "Data Saved: " + msg );
+		  });
+
+	});//onSubmit Form end
+	
+});
