e3252884d09f985d3215139755a8f1a443b807f8
Corey Hobbs <chobberoni@gmail.com>
2016-01-28 17:28:57 -0800

    The beginnings of something better 🙏🏿

--- src/assets/js/checkform.js
+++ src/assets/js/checkform.js
@@ -1,4 +1,43 @@
-$(document).ready(function() {	
+window.onload = function() {
+
+	document.getElementById("category").addEventListener('change',function(e) {ha(e,'secondmenu');}, false);
+	//document.getElementById("secondmenu").addEventListener('change',function(e) {ha(e,'category');}, false);
+};
+
+function ha (e,id){
+	var indexValue = e.target.options[e.target.selectedIndex].value;
+	console.log(index);
+	// if (e.target.selectedIndex == ""
+// 	document.getElementById(id).selectedIndex = index;
+
+	switch(indexValue) {
+		case "1":
+			indexValue = subjOptions.op1;
+			break;
+		case "2":
+			indexValue = subjOptions.op2;
+			break;
+		case "3":
+			indexValue = subjOptions.op3;
+			break;
+		case "4":
+			indexValue = subjOptions.op4;
+			break;
+		case "5":
+			indexValue = subjOptions.op5;
+			break;
+		case "6":
+			indexValue = subjOptions.op6;
+			break;
+		case "7":
+			indexValue = subjOptions.op7;
+			break;
+	}//end of Switch case statement
+}
+
+
+
+$(document).ready(function() {
 	$("#form1").on("submit", function(e) {
 		e.preventDefault();
 
@@ -12,21 +51,22 @@ $(document).ready(function() {
 		datax.comments = $("#comment").val();
 
 		//Validations
-		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/; 
+		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 		//if( !regex.test(datax.email)) { alert('enter a valid email'); }
 
     //Object to send
-    var ddx = { name: datax.name, 
+    var ddx = { name: datax.name,
 		  				email: datax.email ,
 		  				subject: datax.subj,
 		  				comments: datax.comments,
 		  				urlFrom: window.location.pathname
 						};//object
-
-		//Post to Heroku App
+						
+///TEST VERSION IN LOCAL HOST
+		//Post to Heroku App TEST VERSION IN LOCAL HOST
 		$.ajax({
 		  method: "POST",
-		  url: "https://staging-supportal.herokuapp.com",
+		  url: "http://localhost:5000",
 		  dataType: 'json',
 		  cache: false,
 		  data: JSON.stringify(ddx),
@@ -48,8 +88,5 @@ $(document).ready(function() {
 		});//post function
 
 	});//onSubmit Form end
-	
+
 });//end of document.ready Function
-
-
-
