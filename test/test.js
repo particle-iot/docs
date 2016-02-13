2ff6818b28a254d0c44b59fd73599d2416d5341e
Bryce Kahle <bkahle@gmail.com>
2016-02-13 09:58:50 -0800

    attempt to fix test timeouts

--- test/test.js
+++ test/test.js
@@ -40,6 +40,7 @@ describe('Crawler', function() {
     var c = new Crawler({
         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
         maxConnections: 10,
+        agent: false,
         skipDuplicates: true,
         timeout: 3000,
         onDrain: function() {
