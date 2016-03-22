75cc335b9922b305ecc980cafafbbd29ebb5488f
Corey Hobbs <chobberoni@users.noreply.github.com>
2016-03-22 10:40:56 -0700

    Revert "Added Browser & OS Types for Tech Tickets"

--- src/assets/js/checkform.js
+++ src/assets/js/checkform.js
@@ -10,8 +10,6 @@ $(document).ready(function() {
 		datax.email2 = $("#email2").val();
 		datax.subj = $("#category option:selected").val();
 		datax.topic = $("#secondmenu option:selected").val();
-		datax.browserType = browserType;
-		datax.ostype = ostype;
 		datax.comments = $("#comment").val();
 
 		//Validations
@@ -24,11 +22,9 @@ $(document).ready(function() {
 		  				subject: datax.subj,
 						topic: datax.topic,
 		  				comments: datax.comments,
-		  				browserType: datax.browserType,
-		  				osType: datax.ostype,
 		  				urlFrom: window.location.pathname
 						};//object
-		
+
 		$.ajax({
 		  method: "POST",
 		  url: "https://staging-supportal.herokuapp.com",
@@ -43,7 +39,6 @@ $(document).ready(function() {
 					$("#category option:selected").val();
 					$("#secondmenu option:selected").val();
 					$("#comment").val('');
-					$("#fileUp").val();
 					$("#button-blue").css('background-color', 'green');
 					$("#button-blue").attr('value', 'Message Received!');
 			      }
