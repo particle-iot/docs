a6ae22e433fce7278b830f755e21400639a8563c
Bryce Kahle <bkahle@gmail.com>
2016-04-15 12:53:37 -0700

    Use deterministic header ids

--- templates/helpers/permalink-header.js
+++ templates/helpers/permalink-header.js
@@ -1,19 +1,15 @@
 var Handlebars = require('handlebars');
+var crypto = require('crypto');
 
-function makeid()
+
+function makeid(str)
 {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 4; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-    return text;
+	return crypto.createHash('md5').update(str).digest('base64').substr(0, 4).replace(/[+/=]/g, '-');
 }
 
 module.exports = function (str) {
 	var sanitized = str.replace(/[^a-z0-9]/gi, '-').toLowerCase();
-	var anchorId = sanitized + '-' + makeid();
+	var anchorId = sanitized + '-' + makeid(sanitized);
 	var title = Handlebars.Utils.escapeExpression(str);
 
 	return new Handlebars.SafeString("<h3 id='" + sanitized + "' data-href='" + anchorId + "'>" + title + "<a href='#" + sanitized + "' class='header-permalinks' data-menu-href='" + anchorId + "'><i class='ion-link'></i></a></h3>");
