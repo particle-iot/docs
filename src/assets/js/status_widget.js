33d02cf7e72ec93555d7ff87cae709bf6652b5ff
Julien Vanier <julien@particle.io>
2017-03-15 19:38:40 -0400

    Scheduled maintenance shown on support page

--- src/assets/js/status_widget.js
+++ src/assets/js/status_widget.js
@@ -3,15 +3,25 @@
 $('.current-status-indicator').ready(function ($) {
   var colorCodes = {
     none: 'green',
-    minor: 'blue',
-    major: 'yellow',
+    minor: 'yellow',
+    major: 'red',
     critical: 'red'
   };
   var sp = new StatusPage.page({ page : '8d849mp6fy12' });
-  sp.status({
+  sp.summary({
     success: function (data) {
       var description = data.status.description;
       var color = colorCodes[data.status.indicator];
+      var scheduledMaintenance = false;
+      data.scheduled_maintenances.forEach(function (m) {
+        if (m.status == "in_progress") {
+          scheduledMaintenance = true;
+        }
+      });
+      if (scheduledMaintenance) {
+        color = 'blue';
+        description = 'Scheduled maintenance';
+      }
       $('.current-status-description').text(description);
       $('.current-status-indicator').removeClass('empty').addClass(color);
       if (color !== 'green') {
