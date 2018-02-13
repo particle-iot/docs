208992737b0cc23fbf65de8be4124fffebc5dd88
Jeff Eiden <jeff@particle.io>
2018-02-13 06:40:19 -0800

    bump out timeout higher

--- test/crawler.js
+++ test/crawler.js
@@ -29,7 +29,7 @@ function shouldCrawl(qurl) {
 
 describe('Crawler', function() {
   before(function(done) {
-    this.timeout(120000);
+    this.timeout(160000);
     console.log('Building...');
     server = metalsmith.test(done);
   });
