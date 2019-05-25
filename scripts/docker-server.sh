687c73d9e7efa9d13c6ea903afa477c7a1b6bf65
Zachary Crockett <zachary@particle.io>
2019-05-24 17:15:43 -0700

    Assumes script is run from repo root

--- scripts/docker-server.sh
+++ scripts/docker-server.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-if [ -z $REPO_PATH ]; then
-    echo "$0 ERROR: The REPO_PATH environment variable is required!"
-    echo "Try this: export REPO_PATH=\`pwd\`"
-    exit 5
-elif ! [ -d $REPO_PATH ]; then
-    echo "$0 ERROR: $REPO_PATH is not a valid directory"
-    exit 6
-fi
-
-docker run --interactive --mount type=bind,source=$REPO_PATH,target=/particle-iot/docs --net host --rm --tty particle/docs
+docker run --interactive --mount type=bind,source=$(pwd),target=/particle-iot/docs --net host --rm --tty particle/docs "$@"
