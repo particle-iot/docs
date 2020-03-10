9a268d8228f64049a7938c522665be6dee936e17
rickkas7 <rickk@rickk.com>
2020-03-10 13:09:09 -0400

    app note importer

--- scripts/ZendeskMigration/app.js
+++ scripts/ZendeskMigration/app.js
@@ -8,6 +8,8 @@
 const path = require('path'); 
 const fs = require('fs');
 
+const { execFile } = require('child_process');
+
 // https://www.npmjs.com/package/mime-types
 var mime = require('mime-types');
 
@@ -19,6 +21,10 @@ var md = require('markdown-it')({
 	html: true,
 	xhtmlOut: true
 });
+
+// https://github.com/pvorb/node-sha1
+var sha1 = require('sha1');
+
 
 var defaultRenderLinkOpen = md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
 	return self.renderToken(tokens, idx, options);
@@ -49,10 +55,24 @@ md.renderer.rules.image = function (tokens, idx, options, env, self) {
 	srcIndex = token.attrIndex('src');
 
 	if (srcIndex >= 0) {
-		const src = token.attrs[srcIndex][1];
+		let src = token.attrs[srcIndex][1];
 		console.log('image: src=' + src);
 		
-		if (src.startsWith('/assets')) {
+		let importImage = false;
+
+		if (!appNotesMode) {
+			importImage = src.startsWith('/assets');
+		}
+		else {
+			importImage = (src.indexOf('//') < 0);
+
+			if (!src.startsWith('/')) {
+				// URL is relative, so expand it so we have the whole path inside app-notes
+				src = path.dirname(relativePath) + '/' + src;
+			}
+		}
+
+		if (importImage) {
 			// Docs image
 			if (!config.pages[relativePath].attachments) {
 				config.pages[relativePath].attachments = {};
@@ -82,7 +102,8 @@ const Handlebars = require("handlebars");
 
 // md-file should be the path relative to content, for example: support/general/pricing.md
 const argv = require('yargs')
-.usage('Usage: $0 [options] <md-files...>')
+.usage('Usage: $0 [options] [<md-files...>]')
+.describe('a', 'Process app-notes repository instead of md files')
 .argv;
 
 
@@ -101,9 +122,11 @@ if (!ZENDESK_USER) {
 }
 
 
-const srcPath = path.resolve(__dirname, '../../src');
+const topPath = path.resolve(__dirname, '../..');
+const srcPath = path.join(topPath, 'src');
 const assetsPath = path.join(srcPath, 'assets');
 const contentPath = path.join(srcPath, 'content');
+const appNotesPath = path.join(topPath, 'app-notes');
 
 const configPath = path.join(__dirname, 'config.json');
 
@@ -118,6 +141,9 @@ let attachmentsToConvert = [];
 let template;
 let html;
 
+let mdFile;
+
+
 // Axios is used for making direct API calls instead of using node-zendesk 
 const axios = require('axios').default; // https://github.com/axios/axios
 
@@ -128,47 +154,78 @@ let axiosConfig = {
 	}
 };
 
-
 var mdFiles = [];
 
-for(var ii = 0; ii < argv._.length; ii++) {
-	// processFile(argv._[ii]);
+const appNotesMode = argv.a; 
+if (appNotesMode) {
+	console.log('app notes mode');
+
+	if (!fs.existsSync(appNotesPath)) {
+		var args = [
+			'clone',
+			'https://github.com/particle-iot/app-notes.git',
+		];
 	
-	// Argument can be one (or more) files or directories. For directories, all md files in the directory will be updated.
-	// node app.js ../../src/content/support/general/pricing.md
-	// node app.js ../../src/content/support/general
+		var opts = {
+			cwd:topPath
+		};
 	
-	const fileOrDir = path.join(__dirname, argv._[ii]);
+		console.log('git ' + args.join(' '));
 	
-	var stat = fs.statSync(fileOrDir);
-	if (stat.isDirectory()) {
-		var files = fs.readdirSync(fileOrDir);
-		for(var ii = 0; ii < files.length; ii++) {
-			if (files[ii].endsWith('.md')) {
-				// console.log(files[ii]);
-				mdFiles.push(path.join(fileOrDir, files[ii]));
+		execFile('git', args, opts, function(err, stdout, stderr) {
+			if (err) {
+				console.log('git clone failed', err);
+				return;
 			}
-		}
-	}
-	else 
-	if (stat.isFile()) {
-		mdFiles.push(fileOrDir);		
+			console.log('git clone completed!\nstdout:\n' + stdout + '\n\nstderr:\n' + stderr);
+
+			processAppNotes();
+		});
 	}
 	else {
-		console.log('file not found ' + mdFile);
-		process.exit(1);
+		fetchAppNotes();
+		// processAppNotes();
 	}
 }
+else {
+	for(var ii = 0; ii < argv._.length; ii++) {
+		// processFile(argv._[ii]);
+		
+		// Argument can be one (or more) files or directories. For directories, all md files in the directory will be updated.
+		// node app.js ../../src/content/support/general/pricing.md
+		// node app.js ../../src/content/support/general
+		
+		const fileOrDir = path.join(__dirname, argv._[ii]);
+		
+		var stat = fs.statSync(fileOrDir);
+		if (stat.isDirectory()) {
+			var files = fs.readdirSync(fileOrDir);
+			for(var ii = 0; ii < files.length; ii++) {
+				if (files[ii].endsWith('.md')) {
+					// console.log(files[ii]);
+					mdFiles.push(path.join(fileOrDir, files[ii]));
+				}
+			}
+		}
+		else 
+		if (stat.isFile()) {
+			mdFiles.push(fileOrDir);		
+		}
+		else {
+			console.log('file not found ' + mdFile);
+			process.exit(1);
+		}
+	}
 
-if (mdFiles.length == 0) {
-	console.log('no files specified');
-	process.exit(1);	
+	if (mdFiles.length == 0) {
+		console.log('no files specified');
+		process.exit(1);	
+	}
+	
+	
+	// updateArticleURLs();
+	processFile();
 }
-
-
-// updateArticleURLs();
-processFile();
-
 
 // Normally not used, only used to backfill the html_url field
 async function updateArticleURLs() {
@@ -218,21 +275,37 @@ async function processFile() {
 	
 	
 	const mdFilePath = mdFiles.shift();
-	
-	// Relative path should be the filename in src/content without the leading slash
-	// Example: support/particle-devices-faq/wpa2-enterprise.md
-	const relIndex = mdFilePath.indexOf('src/content/');
-	if (relIndex < 0) {
-		console.log('could not decode path ' + mdFilePath);
-		process.exit(1);
-	}
-	relativePath = mdFilePath.substr(relIndex + 12);
-	
-	
-	// Does not contain leading or trailing slash, matches key in sections in config
-	relativeDir = path.dirname(relativePath);
 
+	
+	if (!appNotesPath) {
+		// Relative path should be the filename in src/content without the leading slash
+		// Example: support/particle-devices-faq/wpa2-enterprise.md
+		const relIndex = mdFilePath.indexOf('src/content/');
+		if (relIndex < 0) {
+			console.log('could not decode path ' + mdFilePath);
+			process.exit(1);
+		}
+		relativePath = mdFilePath.substr(relIndex + 12);
+
+		// Does not contain leading or trailing slash, matches key in sections in config
+		relativeDir = path.dirname(relativePath);
+		mdFile = path.join(contentPath, relativePath);
+	}
+	else {
+		// For app notes, relative path is relative to the app-notes directory
+		const relIndex = mdFilePath.indexOf('app-notes/');
+		if (relIndex < 0) {
+			console.log('could not decode path ' + mdFilePath);
+			process.exit(1);
+		}
+		relativePath = mdFilePath.substr(relIndex + 10);
+
+		relativeDir = 'app-notes';
+		mdFile = path.join(appNotesPath, relativePath);
+	}	
+	
 	console.log('processing relativePath=' + relativePath);
+	console.log('mdFile=' + mdFile);
 
 	if (!config.sections[relativeDir]) {
 		console.log('section not defined in config.json for ' + relativeDir);
@@ -258,33 +331,53 @@ async function processFile() {
 	const authorId = config.sections[relativeDir].author;
 	console.log('sectionId=' + sectionId + ' authorId=' + authorId);
 	
-	const mdFile = path.join(contentPath, relativePath);
 
-	let mdContent = fs.readFileSync(mdFile, 'utf8')
+	let mdContent = fs.readFileSync(mdFile, 'utf8');
+
+	const hash = sha1(mdContent);
+	if (config.pages[relativePath]['hash'] === hash) {
+		console.log('md file unchanged, not updating ' + relativePath);
+		processFile();
+		return;
+	}
+	config.pages[relativePath]['hash'] = hash;
+	saveConfig();				
 
 	let desc = {};
 
-
-	let offset1 = mdContent.indexOf('---');
-	if (offset1 >= 0) {
-		offset1 += 4;
-		let offset2 = mdContent.indexOf('---', offset1);
-		if (offset2 >= 0) {
-			const descBlock = mdContent.substr(offset1, offset2 - offset1);
-			
-			descBlock.split(/\n/).forEach(function(line) {
-				// console.log('desc line ' + line);
-				const colonIndex = line.indexOf(':');
-				if (colonIndex >= 1) {
-					const key = line.substr(0, colonIndex).trim();
-					const value = line.substr(colonIndex + 1).trim();
-					desc[key] = value;
-				}
-			});
-			mdContent = mdContent.substr(offset2 + 4); 
+	if (!appNotesMode) {
+		let offset1 = mdContent.indexOf('---');
+		if (offset1 >= 0) {
+			offset1 += 4;
+			let offset2 = mdContent.indexOf('---', offset1);
+			if (offset2 >= 0) {
+				const descBlock = mdContent.substr(offset1, offset2 - offset1);
+				
+				descBlock.split(/\n/).forEach(function(line) {
+					// console.log('desc line ' + line);
+					const colonIndex = line.indexOf(':');
+					if (colonIndex >= 1) {
+						const key = line.substr(0, colonIndex).trim();
+						const value = line.substr(colonIndex + 1).trim();
+						desc[key] = value;
+					}
+				});
+				mdContent = mdContent.substr(offset2 + 4); 
+			}
 		}
 	}
+	else {
+		let offset1 = mdContent.indexOf('# ');
+		if (offset1 >= 0) {
+			let offset2 = mdContent.indexOf('\n', offset1);
+			if (offset2 >= 0) {
+				desc['title'] = mdContent.substr(offset1 + 2, offset2 - offset1 - 2).trim();
+			}
+		}
+
+	}
 	console.log('title=' + desc['title']);
+
 	handlebarsEnv.title = desc['title'];
 	
 	// Before parsing the 
@@ -366,7 +459,15 @@ async function uploadAttachments() {
 
 	console.log('processing attachment ' + src + ' contentType=' + contentType);
 	
-	const imageFile = fs.readFileSync(path.join(srcPath, src));
+	let imagePath;
+	if (!appNotesMode) {
+		imagePath = path.join(srcPath, src);
+	}
+	else {
+		imagePath = path.join(appNotesPath, src);
+	}
+
+	const imageFile = fs.readFileSync(imagePath);
 
 	//const imageFileBase64 = imageFile.toString('base64');
 	
@@ -467,4 +568,72 @@ function saveConfig() {
 	const configText = JSON.stringify(config, null, 2);
 	
 	fs.writeFileSync(configPath, configText);
+}
+
+
+function fetchAppNotes() {
+	var args = [
+		'fetch'
+	];
+
+	var opts = {
+		cwd:appNotesPath
+	};
+
+	console.log('git ' + args.join(' '));
+
+	execFile('git', args, opts, function(err, stdout, stderr) {
+		if (err) {
+			console.log('git fetch failed', err);
+			return;
+		}
+		console.log('git fetch completed!\nstdout:\n' + stdout + '\n\nstderr:\n' + stderr);
+
+		mergeAppNotes();
+	});
+}
+
+function mergeAppNotes() {
+	var args = [
+		'merge',
+		'origin/master'
+	];
+
+	var opts = {
+		cwd:appNotesPath
+	};
+
+	console.log('git ' + args.join(' '));
+
+	execFile('git', args, opts, function(err, stdout, stderr) {
+		if (err) {
+			console.log('git merge failed', err);
+			return;
+		}
+		console.log('git merge completed!\nstdout:\n' + stdout + '\n\nstderr:\n' + stderr);
+
+		processAppNotes();
+	});
+}
+
+async function processAppNotes() {
+	// Not yet implemented!
+
+	
+	var files = fs.readdirSync(appNotesPath);
+	for(var ii = 0; ii < files.length; ii++) {
+		const thisAppNotePath = path.join(appNotesPath, files[ii]);
+		var stat = fs.statSync(thisAppNotePath);
+		if (stat.isDirectory()) {
+			if (files[ii].startsWith('AN')) {
+				var thisMdPath = path.join(thisAppNotePath, 'README.md');
+				if (fs.existsSync(thisMdPath)) {
+					// console.log(thisAppNotePath);
+					mdFiles.push(thisMdPath);
+				}
+			}
+		}		
+	}
+	processFile();
+
 }
