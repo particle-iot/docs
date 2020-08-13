96fc360f086e86dee6968ab30812cff43e17fb85
rickkas7 <rickk@rickk.com>
2020-08-13 14:01:33 -0400

    Image overlay support

---
+++ templates/helpers/imageOverlay.js
@@ -0,0 +1,26 @@
+var crypto = require('crypto');
+
+var Handlebars = require('handlebars');
+
+module.exports = function(context) {
+	var html = '';
+
+    var src = context.hash.src;
+    var large = context.hash.large || src;
+    var alt = context.hash.alt;
+    var cssClass = context.hash.class;
+	
+	var html = '';
+
+    
+    html += '<div align="center"><img src="' + src + '" onclick="showOverlay(\'' + large + '\')" style="cursor:zoom-in" ';
+    if (alt) {
+        html += ' alt="' + alt +'" ';
+    }
+    if (cssClass) {
+        html += ' class="' + cssClass + '" ';
+    }
+    html += '></div>';
+	
+	return new Handlebars.SafeString(html);
+};
