dd4f3f1c44a43a5e13639e2bef5450bc4b0b290e
rickkas7 <rickk@rickk.com>
2021-03-29 17:14:51 -0400

    BLE for Device OS 3.0 Updates

---
+++ src/assets/files/ble/device-nearby-beacon.cpp
@@ -0,0 +1,59 @@
+#include "Particle.h"
+
+// This example does not require the cloud so you can run it in manual mode or
+// normal cloud-connected mode
+// SYSTEM_MODE(MANUAL);
+
+SerialLogHandler logHandler(LOG_LEVEL_TRACE);
+
+const uint32_t myColor = 0xff0000;
+// 0xff0000 = red
+// 0x00ff00 = green
+// 0x0000ff = blue
+
+void setAdvertisingData();
+
+void setup() {
+	(void)logHandler; // Does nothing, just to eliminate the unused variable warning
+
+	BLE.on();
+
+	setAdvertisingData();
+}
+
+void loop() {
+
+}
+
+void setAdvertisingData() {
+	uint8_t buf[BLE_MAX_ADV_DATA_LEN];
+
+	size_t offset = 0;
+
+	// Manufacturer-specific data
+	// 16-bit: Company ID (0xffff)
+	// Byte: Internal packet identifier (0x55)
+	// 32-bit: Color code
+
+	// Company ID (0xffff internal use/testing)
+	buf[offset++] = 0xff;
+	buf[offset++] = 0xff;
+
+	// Internal packet type. This is arbitrary, but provides an extra
+	// check to make sure the data is my data, since we use the 0xffff company
+	// code.
+	buf[offset++] = 0x55;
+
+	// Our specific data, color code
+	memcpy(&buf[offset], &myColor, 4);
+	offset += 4;
+
+	BleAdvertisingData advData;
+	advData.appendCustomData(buf, offset);
+
+	// Advertise every 100 milliseconds. Unit is 0.625 millisecond intervals.
+	BLE.setAdvertisingInterval(130);
+
+	// Continuously advertise
+	BLE.advertise(&advData);
+}
---
+++ src/assets/files/ble/uart-peripheral.cpp
@@ -0,0 +1,54 @@
+#include "Particle.h"
+
+// This example does not require the cloud so you can run it in manual mode or
+// normal cloud-connected mode
+// SYSTEM_MODE(MANUAL);
+
+const size_t UART_TX_BUF_SIZE = 20;
+
+void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context);
+
+// These UUIDs were defined by Nordic Semiconductor and are now the defacto standard for
+// UART-like services over BLE. Many apps support the UUIDs now, like the Adafruit Bluefruit app.
+const BleUuid serviceUuid("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+const BleUuid rxUuid("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+const BleUuid txUuid("6E400003-B5A3-F393-E0A9-E50E24DCCA9E");
+
+BleCharacteristic txCharacteristic("tx", BleCharacteristicProperty::NOTIFY, txUuid, serviceUuid);
+BleCharacteristic rxCharacteristic("rx", BleCharacteristicProperty::WRITE_WO_RSP, rxUuid, serviceUuid, onDataReceived, NULL);
+
+void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context) {
+    // Log.trace("Received data from: %02X:%02X:%02X:%02X:%02X:%02X:", peer.address()[0], peer.address()[1], peer.address()[2], peer.address()[3], peer.address()[4], peer.address()[5]);
+
+    for (size_t ii = 0; ii < len; ii++) {
+        Serial.write(data[ii]);
+    }
+}
+
+void setup() {
+    Serial.begin();
+
+	BLE.on();
+
+    BLE.addCharacteristic(txCharacteristic);
+    BLE.addCharacteristic(rxCharacteristic);
+
+    BleAdvertisingData data;
+    data.appendServiceUUID(serviceUuid);
+    BLE.advertise(&data);
+}
+
+void loop() {
+    if (BLE.connected()) {
+    	uint8_t txBuf[UART_TX_BUF_SIZE];
+    	size_t txLen = 0;
+
+    	while(Serial.available() && txLen < UART_TX_BUF_SIZE) {
+            txBuf[txLen++] = Serial.read();
+            Serial.write(txBuf[txLen - 1]);
+        }
+        if (txLen > 0) {
+            txCharacteristic.setValue(txBuf, txLen);
+        }
+    }
+}
