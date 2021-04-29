56f8f12032fd439eb449b2a22cd8f494ff84f079
rickkas7 <rickk@rickk.com>
2021-04-29 11:54:20 -0400

    checkpoint
    - not functional
    [ci skip]

--- src/assets/js/api-helper-usb.js
+++ src/assets/js/api-helper-usb.js
@@ -6,13 +6,14 @@ $(document).ready(function () {
     const noWebUsbError = 'Your web browser does not support WebUSB and cannot access Particle devices using USB.<br/>' +
         'One alternative is to use the <a href="/tutorials/developer-tools/cli/">Particle CLI</a>.';
 
+    let deviceRestoreInfo;
 
     if ($('.apiHelperUsbRestoreDevice').each(function() {
         const thisPartial = $(this);
 
         const selectElem = $(thisPartial).find('.apiHelperUsbRestoreDeviceSelect');
         const versionElem = $(thisPartial).find('.apiHelperUsbRestoreDeviceVersion');
-        const restoreElem = $(thisPartial).find('.apiHelperUsbRestoreDeviceVersion');
+        const restoreElem = $(thisPartial).find('.apiHelperUsbRestoreDeviceRestore');
 
         const setStatus = function(str) {
             $(thisPartial).find('.apiHelperUsbRestoreDeviceStatus').html(str);
@@ -25,6 +26,7 @@ $(document).ready(function () {
         }
 
         let usbDevice;
+        let platformObj;
 
         if ($(selectElem).on('click', async function () {
             const filters = [
@@ -43,6 +45,26 @@ $(document).ready(function () {
                 console.log('usbDevice', usbDevice);
         
                 // Find available versions for this device
+                let versionArray;
+
+                for(let tempPlatformObj of deviceRestoreInfo.platforms) {
+                    if (tempPlatformObj.id == usbDevice.platformId) {
+                        if (deviceRestoreInfo.versionsByPlatform[tempPlatformObj.name]) {
+                            versionArray = deviceRestoreInfo.versionsByPlatform[tempPlatformObj.name];
+                            platformObj = tempPlatformObj;
+                            break;
+                        }
+                    }
+                }
+                
+                if (!versionArray) {
+                    setStatus('No device restore images found for this device');
+                    return;
+                }
+                $(versionElem).html('');
+                for(let ver of versionArray) {
+                    versionElem.append('<option name="' + ver + '">' + ver + '</option>');
+                }
                 
                 $(versionElem).prop('disabled', false);
                 $(restoreElem).prop('disabled', false);
@@ -53,8 +75,127 @@ $(document).ready(function () {
             }        
         }));
 
+
+        const calculateBufferChecksum = function(buf) {
+            // Last byte of buf is the checksum, so don't include that in
+            // the checksum (buf.length - 1)
+            let sum = 0;
+            for(let ii = 0; ii < (buf.length - 1); ii++) {
+                sum += buf[ii];
+            }
+        
+            // Checksum is sum of all bytes, then two's complement
+            // (invert all of the bits and add 1)
+            return (~sum + 1) & 0xff;
+        }
+                
+
         if ($(restoreElem).on('click', async function () {
-            console.log('restore!');
+            const version = $(versionElem).val();
+
+            const hexUrl = '/assets/files/device-restore/' + version + '/' + platformObj.name + '.hex';
+
+            setStatus('Downloading restore image...');
+
+            const hexTextResp = await fetch(hexUrl);
+            const hexText = await hexTextResp.text();
+    
+            setStatus('Processing restore image...');
+
+            let baseAddr = 0;
+        
+            let fullMemoryMap = new Uint8Array(1024 * 1024);
+
+            hexText.split(/[\r\n]/).forEach(function(lineData) {        
+                lineData = lineData.trim();
+                if (lineData.length == 0) {
+                    return;
+                }
+                if (lineData.charAt(0) != ':') {
+                    return;
+                }
+        
+                const buf = new Uint8Array(lineData.substr(1).match(/.{1,2}/g).map(byte => parseInt(byte, 16)))
+               
+                const len = buf[0]; 
+                const addr = (buf[1] << 8) | buf[2]; // Big Endian 16-bit
+                const recType = buf[3];
+                // data begins at 4
+                // checksum is last byte
+                const checksum = buf[4 + len];
+                
+                const calcChecksum = calculateBufferChecksum(buf);
+        
+                if (calcChecksum == checksum) {
+                    if (recType == 4 && len == 2) {
+                        // Extended linear address
+                        baseAddr = (buf[4] << 8) | buf[5]; // Big Endian 16-bit
+                        baseAddr <<= 16;
+                        // console.log('baseAddr=0x' + baseAddr.toString(16) + ' (' + baseAddr + ')');
+                    }
+
+                    // STM32 devices have 0x8000000 as the address of flash, and we want to ignore that
+                    baseAddr &= 0x07ffffff; 
+
+                    if (baseAddr < fullMemoryMap.byteLength) {
+                        for(let ii = 0; ii < len; ii++) {
+                            fullMemoryMap[baseAddr + addr + ii] = buf[4 + ii];
+                        }                
+                    }
+                    else {
+                        console.log('ignoring '+ (baseAddr + addr));
+                    }
+                    
+                    // console.log('len=0x' + padHex(len, 2) + ' addr=0x' + padHex(addr, 4) + ' recType=' + recType + ' checksum=0x' + padHex(checksum, 2) + ' ' + lineData);
+                }
+                else {
+                    // console.log('CHECKSUM ERROR! len=' + len + ' addr=' + addr + ' recType=' + recType + ' checksum=' + checksum + ' calcChecksum=' + calcChecksum);
+                }
+            });
+        
+            setStatus('Preparing to flash...');
+
+            if (platformObj.id == 6 || platformObj.id == 8) {
+                // Photon/P1
+                let part;
+
+                // System Part 1	0x8020000	0x8060000	256 KB
+                setStatus('Flashing system part 1...');
+                part = new Uint8Array(256 * 1024);
+                for(let ii = 0; ii < 256 * 1024; ii++) {
+                    part[ii] = fullMemoryMap[0x20000 + ii];
+                }
+                try {
+                    await usbDevice.updateFirmware(part, {});
+                }
+                catch(e) {
+                    console.log('exception', e);
+                }
+
+                // System Part 2	0x8060000	0x80A0000	256 KB
+                part = new Uint8Array(256 * 1024);
+                for(let ii = 0; ii < 256 * 1024; ii++) {
+                    part[ii] = fullMemoryMap[0x60000 + ii];
+                }
+                await usbDevice.updateFirmware(part, {});
+
+                // Bootloader	0x8000000	0x8004000	16 KB
+
+                // User Part	0x80A0000	0x80C0000	128 KB
+                part = new Uint8Array(128 * 1024);
+                for(let ii = 0; ii < 128 * 1024; ii++) {
+                    part[ii] = fullMemoryMap[0xa0000 + ii];
+                }
+                await usbDevice.updateFirmware(part, {});
+            }
+            else
+            if (platformObj.id == 10) {
+                // Electron/E Series
+            }
+            else {
+                // Gen 3
+
+            }
 
         }));
     }));
@@ -112,7 +253,8 @@ $(document).ready(function () {
             }
             catch(e) {
                 console.log('no device selected', e);
-            }        }));
+            }        
+        }));
     }));
 
 
@@ -133,6 +275,13 @@ $(document).ready(function () {
             }        
         }));
     }));
+
+    fetch('/assets/files/deviceRestore.json')
+    .then(response => response.json())
+    .then(function(res) {
+        deviceRestoreInfo = res;
+        // console.log('deviceRestoreInfo', deviceRestoreInfo);
+    });
 
 
 });
