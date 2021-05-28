d22c1c369aed605dd19f9e1a728ce1ded19d1afa
rickkas7 <rickk@rickk.com>
2021-05-28 09:29:23 -0400

    Project browser
    - Not currently used, will be used in upcoming tutorials
    - Allows projects of more than one file to be easily viewed and downloaded as a zip
    - Also works to allow flash to a device of a multi-file project

--- src/assets/js/api-helper-projects.js
+++ src/assets/js/api-helper-projects.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     }
     $('.apiHelperProjectBrowser').each(function() {
         const thisElem = $(this);
+        const gaCategory = 'Project Browser';
 
         const project = $(thisElem).data('project');
         const defaultFile = $(thisElem).data('default-file');
@@ -28,8 +29,7 @@ $(document).ready(function() {
 
         let projectZip;
 
-        $(fileSelect).on('click', async function() {
-            // Load the zip on first click
+        const getProjectZip = async function() {
             if (!projectZip) {
                 projectZip = new zip.fs.FS();
 
@@ -60,7 +60,16 @@ $(document).ready(function() {
                 }
                 addDir('', projectZip.root.children[0]);
             }
+            return projectZip;
+        }
+
+        $(fileSelect).on('click', async function() {
+            // Load the zip on first click
+            getProjectZip();
         });
+
+        let curText;
+        let curPath;
 
         const showFile = function(path, text) {
             let name, ext = '';
@@ -76,6 +85,9 @@ $(document).ready(function() {
                 ext = name.substr(index + 1);
             }
             ext = ext.toLowerCase();
+
+            curText = text;
+            curPath = path;
 
             $(outputCodeElem).hide();
             $(outputPreElem).hide();
@@ -100,12 +112,134 @@ $(document).ready(function() {
         $(fileSelect).on('change', async function() {
             const path = project + '/' + $(fileSelect).val();
 
+            setStatus('Opening ' + path + '...');
+
             const ze = projectZip.find(path);
 
             const text = await ze.getText();
 
-            showFile(path, text);
+            const fullUrlPath = projectUrlBase + '/' + $(fileSelect).val();
+            showFile(fullUrlPath, text);
+
+			ga('send', 'event', gaCategory, 'View File', path);
+            setStatus('');
         });
+
+
+
+		$(thisElem).find('.apiHelperProjectDownloadButton').on('click', function() {	
+			var a = document.createElement('a');
+			a.href = curPath;
+			a.download = curPath.split('/').pop();
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+
+			ga('send', 'event', gaCategory, 'File Download', curPath);
+		});
+
+		$(thisElem).find('.apiHelperProjectCopyButton').on('click', function() {			
+			var t = document.createElement('textarea');
+			document.body.appendChild(t);
+			$(t).text(curText);
+			t.select();
+			document.execCommand("copy");
+			document.body.removeChild(t);
+
+			ga('send', 'event', gaCategory, 'File Copy', curPath);
+		});
+
+        $(thisElem).find('.apiHelperProjectDownloadZipButton').on('click', function() {
+			var a = document.createElement('a');
+			a.href = projectUrlBase + '.zip';
+			a.download = project + '.zip';
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+
+			ga('send', 'event', gaCategory, 'Zip Download', project);
+        });
+
+        const flashDeviceButton = $(thisElem).find('.codeboxFlashDeviceButton');
+
+        $(flashDeviceButton).on('click', async function() {
+
+			const device = $(thisElem).find('select.codeboxFlashDeviceSelect').val();
+			if (!device || device == 'select') {
+				return;
+			}
+		
+            if (!apiHelper.confirmFlash()) {
+                return;
+            }
+		
+            $(flashDeviceButton).prop('disabled', true);
+            
+            await getProjectZip();
+
+            let formData = new FormData();
+
+            formData.append('deviceId', device);
+            // formData.append('build_target_version', 'latest');
+            let fileNum = 0;
+
+            const addDir = async function(path, zipDir) {
+                for(const d of zipDir.children) {
+                    const p = (path ? path + '/' : '') + d.name;
+                    if (d.directory) {
+                        await addDir(p, d);
+                    }
+                    else {
+                        const blob = await d.getBlob('text/plain');
+                        formData.append('file' + (++fileNum), blob, p);
+                    }
+                }
+            }
+            await addDir('', projectZip.root.children[0]);
+
+            setStatus('Starting compile and flash...');
+            let startTimer = setTimeout(function() {
+                setStatus('Compile still in progress...');
+                startTimer = null;
+            }, 3000);
+
+            const request = {
+                contentType: false,
+                data: formData,
+                dataType: 'json',
+                error: function (jqXHR) {
+                    ga('send', 'event', gaCategory, 'Flash Device Error', (jqXHR.responseJSON ? jqXHR.responseJSON.error : ''));
+                    if (startTimer) {
+                        clearTimeout(startTimer);
+                        startTimer = null;
+                    }
+                    setStatus('Compile and flash failed');
+                    $(flashDeviceButton).prop('disabled', false);
+                },
+                headers: {
+                    'Authorization': 'Bearer ' + apiHelper.auth.access_token,
+                    'Accept': 'application/json'
+                },
+                method: 'PUT',
+                processData: false,
+                success: function (resp, textStatus, jqXHR) {
+                    ga('send', 'event', gaCategory, 'Flash Device Success');
+                    if (startTimer) {
+                        clearTimeout(startTimer);
+                        startTimer = null;
+                    }
+                    setStatus('Compile succeeded, flash started!');
+                    $(flashDeviceButton).prop('disabled', false);
+
+                    setTimeout(function() {
+                        setStatus('');
+                    }, 8000);                
+                },
+                url: 'https://api.particle.io/v1/devices/' + device
+            };
+
+            $.ajax(request);
+		});
 
 
         const showDefaultFile = async function() {
