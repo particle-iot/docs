7d4765d213ba594c7d34a9fc64d762bd7a746e71
rickkas7 <rickk@rickk.com>
2021-06-17 11:15:00 -0400

    Try running deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+if [ "${CIRCLE_PULL_REQUEST}" != "false" ]; then
 	exit 0
 fi
 
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
 	BUCKET="docs.particle.io"
-elif [ "${TRAVIS_BRANCH}" == "staging" ]; then
+elif [ "${CIRCLE_BRANCH}" == "staging" ]; then
 	BUCKET="docs.staging.particle.io"
-elif [ "${TRAVIS_BRANCH}" == "prerelease" ]; then
+elif [ "${CIRCLE_BRANCH}" == "prerelease" ]; then
 	BUCKET="prerelease-docs.particle.io"
 	AWS_ACCESS_KEY_ID=$PRERELEASE_AWS_ACCESS_KEY_ID
 	AWS_SECRET_ACCESS_KEY=$PRERELEASE_AWS_SECRET_ACCESS_KEY
@@ -23,4 +23,3 @@ set -e
 pip install --user s3cmd
 s3cmd sync --no-preserve --no-progress --acl-public --delete-removed --no-mime-magic --guess-mime-type --add-header="Cache-Control:max-age=300" --exclude='search-index.json' build/ "s3://${BUCKET}/"
 
-#s3cmd sync --no-preserve --no-progress --acl-public --no-mime-magic --guess-mime-type --add-header="Cache-Control:max-age=300" --add-header="Content-Encoding:gzip" build/search-index.json "s3://${BUCKET}/"
