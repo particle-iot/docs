3c9a9378d6fa5f65b5e5d71ab4c71eca8916bcd5
rickkas7 <rickk@rickk.com>
2021-10-31 12:12:03 -0400

    checkpoint
    - work in progress

--- src/assets/js/device-setup-usb.js
+++ src/assets/js/device-setup-usb.js
@@ -66,6 +66,10 @@ $(document).ready(function() {
         
                 usbDevice = await ParticleUsb.openDeviceById(nativeUsbDevice, {});
 
+                // TODO: Try this with a device with old Device OS, not sure whether this step
+                // fails or the next one, but if the Device OS doesn't support control requests
+                // we need to update Device OS first, then go back to check device settings.
+
                 checkDevice();
             }
             catch(e) {
@@ -83,6 +87,7 @@ $(document).ready(function() {
                 deviceInfo.deviceId = usbDevice.id;
                 deviceInfo.platformId = usbDevice.platformId;
                 deviceInfo.firmwareVersion = usbDevice.firmwareVersion;
+                deviceInfo.platformVersionInfo = apiHelper.getRestoreVersions(usbDevice);
 
                 
                 if (usbDevice.isCellularDevice) {
@@ -97,22 +102,147 @@ $(document).ready(function() {
 
                 }
 
+
             }
             catch(e) {
                 console.log('exception', e);
             }
         };
 
+        const checkSim = async function() {
+
+        };
+
         const activateSim = async function() {
-            try {
-                setSetupStep('setupStepActivateSim');
+            setSetupStep('setupStepActivateSim');
 
-                setStatus('Activating SIM...');
-            }
-            catch(e) {
-                console.log('exception', e);
+            let needToActivate = false;
+            let alreadyOwned = false;
+            let clockTimer;
+            let clockStart;
+
+            const startClock = function() {
+                $(thisElem).find('.setupStepActivateSimWaiting').show();
+
+                clockTimer = setInterval(function() {
+                    const elapsedMs = new Date().getTime() - clockStart.getTime();
+
+                    $(thisElem).find('.setupStepActivateSimTimer').text(elapsedMs / 1000);
+                }, 1000);
+            };
+
+            while(true) {
+                try {
+                    setStatus('Checking SIM...');
+    
+                    // listSIMs doesn't filter on iccid correctly
+                    // checkSIM does a HEAD so it doesn't return enough useful information
+                    const result = await new Promise(function(resolve, reject) {
+    
+                        const request = {
+                            dataType: 'json',
+                            error: function (jqXHR) {
+                                console.log('error', jqXHR);
+                                reject(jqXHR.status);
+                            },
+                            headers: {
+                                'Authorization': 'Bearer ' + apiHelper.auth.access_token,
+                                'Accept': 'application/json'
+                            },
+                            method: 'GET',
+                            success: function (resp, textStatus, jqXHR) {
+                                resolve(resp);
+                            },
+                            url: 'https://api.particle.io/v1/sims/' + deviceInfo.iccid
+                        };
+            
+                        $.ajax(request);            
+                    });
+    
+                    /*
+                    last_status_change_action: "activate"
+                    last_status_change_action_error: "no"
+                    last_status_change_action_on: "2021-10-31T11:44:01.655Z"
+                    state_change_in_progress: false
+                    status: "active"
+                    */
+    
+                    alreadyOwned = true;
+                    if (result.status != 'active') {
+                        if (result.last_status_change_action == 'activate' && result.state_change_in_progress) {
+                            console.log('activation in progress', result);
+                            setStatus('SIM is being activated...');
+                            if (!clockStart) {
+                                clockStart = new Date(result.last_status_change_action_on);
+                                startClock();
+                            }
+                        }
+                        else {
+                            console.log('need to activate', result)
+                            needToActivate = true;
+                        }
+                    }
+                    else {
+                        console.log('active already', result);
+                        flashDevice();
+                        break;
+                    }                            
+    
+                    // result.statusCode is 200 if not activated, and 205 if activated? Weird.
+                }
+                catch(e) {
+                    if (e == 404) {
+                        // 
+                        console.log('SIM not active in account, need to activate');
+                        needToActivate = true;
+                    }
+                    else {
+                        console.log('exception', e);
+                        break;    
+                    }
+                }
+                
+    
+                if (needToActivate) {
+                    try {
+                        setStatus('Activating SIM...');
+
+                        clockStart = new Date();
+                        startClock();
+
+                        const result = await apiHelper.particle.activateSIM({ auth: apiHelper.auth.access_token, iccid: deviceInfo.iccid});
+        
+                        console.log('result', result);
+                    }
+                    catch(e) {
+                        if (e.message.includes('408')) {
+                            console.log('408 - activation in progress');
+                            continue;
+                        }
+                        else
+                        if (e.message.includes('401')) {
+                            console.log('401 error - expired token?');
+                        }
+                        else {
+                            console.log('exception', e);
+                        }
+                        break;
+                    }    
+                }
+                else {
+                    // Wait a bit to try again
+                    await new Promise(function(resolve) {
+                        setTimeout(function() {
+                            resolve();
+                        }, 30000);
+                    });
+                }
+                
             }
 
+            if (clockTimer) {
+                clearInterval(clockTimer);
+            }
             
         };
 
@@ -120,12 +250,77 @@ $(document).ready(function() {
             try {
                 setSetupStep('setupStepFlashDevice');
 
+                // Prepare firmware
+
+                // Flash device
                 setStatus('Flashing device...');
+                        
+                let options = {
+                    eventCategory: 'USB Device Setup',
+                    platformVersionInfo: deviceInfo.platformVersionInfo = apiHelper.getRestoreVersions(usbDevice),
+                    setStatus,
+                    version: '2.2.0', // FIXME: Latest LTS? Or make selectable?
+                    setupBit: 'done',
+                    progressUpdate: function(msg, pct) {
+                        $(thisElem).find('.setupStepFlashDeviceStatus').text(msg);
+                        $(thisElem).find('.setupStepFlashDeviceProgress').val(pct);
+                    }
+                };
+
+            
+                const restoreResult = await dfuDeviceRestore(usbDevice, options);
+
+                console.log('restoreResult', restoreResult);
+
+                if (restoreResult.ok) {
+                    // If this is a cellular device, configureWiFi() jumps immediately into waitDeviceOnline()
+                    configureWiFi();      
+                }
+                else {
+                    console.log('do something for dfu error');
+                }
                 
             }
             catch(e) {
                 console.log('exception', e);
             }
+        };
+
+        const configureWiFi = async function() {
+            if (!deviceInfo.wifi) {
+                waitDeviceOnline();
+                return;
+            }
+
+            setSetupStep('setupStepConfigureWiFi');
+
+            // Start Wi-Fi scan
+
+            // Display results
+
+            // Set Wi-Fi
+
+        };
+
+        const waitDeviceOnline = async function() {
+            setSetupStep('setupStepWaitForOnline');
+
+            // Wait for online
+            
+            
+        };
+
+        const claimAndNameDevice = async function() {
+            setSetupStep('setupStepClaimAndNameDevice');
+
+            
+        };
+
+        const setupDone = async function() {
+            setSetupStep('setupStepDone');
+            
+
+
         };
     });
 
