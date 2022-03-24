49b69626c3b4fd5728de1521344f0baa9b7f4f2a
rickkas7 <rickk@rickk.com>
2022-03-24 10:28:55 -0400

    checkpoint
    - update node tutorial examples

--- src/assets/files/projects/node-list-devices5/app.js
+++ src/assets/files/projects/node-list-devices5/app.js
@@ -1,24 +1,30 @@
 var Particle = require('particle-api-js');
 var particle = new Particle();
 
-const accessToken = process.env.PARTICLE_AUTH;
-if (!accessToken) {
-    console.log('You must specify the PARTICLE_AUTH environment variable');
-    process.exit(1);
-}
+const helper = require('@particle/node-example-helper');
 
 async function run() {
-    const devices = await particle.listDevices({ auth: accessToken });
+    // Authenticate the user and obtain the access token
+    await helper.authenticate();
 
-    const oldestTime = Date.parse('01 Jan 2021');
+    try {
+        // Get the list of devices
+        const devices = await particle.listDevices({ auth: helper.auth });
 
-    for(const dev of devices.body) {
-        const lastHeard = Date.parse(dev.last_heard);
+        const oldestTime = Date.parse('01 Jan 2021');
 
-        if (lastHeard < oldestTime) {
-            console.log('name=' + dev.name + ' id=' + dev.id);
+        for(const dev of devices.body) {
+            const lastHeard = Date.parse(dev.last_heard);
+
+            if (lastHeard < oldestTime) {
+                console.log('name=' + dev.name + ' id=' + dev.id);
+            }
         }
     }
+    catch(e) {
+        console.log('exception getting device list', e);
+    }
+    helper.close();
 }
 
 run();
--- src/assets/files/projects/node-list-devices6/app.js
+++ src/assets/files/projects/node-list-devices6/app.js
@@ -1,23 +1,30 @@
 var Particle = require('particle-api-js');
 var particle = new Particle();
 
-const accessToken = process.env.PARTICLE_AUTH;
-if (!accessToken) {
-    console.log('You must specify the PARTICLE_AUTH environment variable');
-    process.exit(1);
-}
+const helper = require('@particle/node-example-helper');
 
 async function run() {
-    const devices = await particle.listDevices({ auth: accessToken });
+    // Authenticate the user and obtain the access token
+    await helper.authenticate();
 
-    console.log('deviceId,iccid');
-    
-    for(const dev of devices.body) {
-        if (dev.iccid) {
-            console.log(dev.id + ',' + dev.iccid);
+    try {
+        // Get the list of devices
+        const devices = await particle.listDevices({ auth: helper.auth });
+
+        console.log('deviceId,iccid');
+        
+        for(const dev of devices.body) {
+            if (dev.iccid) {
+                console.log(dev.id + ',' + dev.iccid);
+            }
         }
     }
+    catch(e) {
+        console.log('exception getting device list', e);
+    }
+    helper.close();
 }
+
 
 run();
 
