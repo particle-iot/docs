aa6b691bb058fba3cd6eda76bc6c72a925adc747
rickkas7 <rickk@rickk.com>
2022-04-28 08:49:59 -0400

    checkpoint
    - work in progress

---
+++ src/assets/js/search.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+    const engineKey = 'xqs6ySrzs42txxzuWP_A';
+
+    const resultTemplate = Handlebars.compile('<div class="st-result"><h3 class="title"><a href="{{url}}" class="st-search-result-link">{{title}}</a></h3><div class="st-metadata"><span class="st-snippet">{{{body}}}</span></div></div>');
+        
+
+    const customRenderer = function(documentType, item) {
+        var data = {
+          title: item['title'],
+          url: item['url'],
+          body: item.highlight['body']
+        };
+        return resultTemplate(data);
+    };
+
+    $('#st-search-input').on('keydown', function(ev) {
+        if (ev.key != 'Enter') {
+            return;
+        }
+
+        const searchText = $('#st-search-input').val();
+        if (searchText) {
+            ev.preventDefault();
+            const searchTextEnc = encodeURI(searchText);
+            location.href = '/search?#stq=' + searchTextEnc + '&stp=1';    
+        }
+    });
+
+    $('#st-search-input').swiftype({
+        // Autocomplete
+        engineKey
+    });
+    $('#st-search-input-page').swiftypeSearch({
+        // Form-based
+        resultContainingElement: '#st-results-container',
+        renderFunction: customRenderer,
+        engineKey
+    });
+
+    if (window.location.hash && window.location.hash.startsWith('#stq=')) {
+        // Search parameters in URL
+        let searchTerm = window.location.hash.substring(5); // Skip over #stq=
+        const sepIndex = searchTerm.indexOf('&');
+        if (sepIndex >= 0) {
+            searchTerm = searchTerm.substring(0, sepIndex);
+        }
+        try {
+            searchTerm = decodeURI(searchTerm);
+            $('#st-search-input-page').val(searchTerm);    
+        }
+        catch(e) {
+
+        }
+    }
+});    
