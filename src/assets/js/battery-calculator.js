04ef3f67124dd1bd76ea3fdad888c7cfbc0d5dc1
rickkas7 <rickk@rickk.com>
2023-01-10 05:37:00 -0500

    checkpoint
    - work in progress

---
+++ src/assets/js/battery-calculator.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // auth not required
+
+    // Possibly move this to a JSON file
+    const batteryInfo = {
+        devices: [
+            
+        ]
+    }
+
+    $('.batteryCalculator').each(function() {
+        const thisElem = $(this);
+        console.log('batteryCalculator');
+    });
+});
