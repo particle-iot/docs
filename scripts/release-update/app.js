0d0858d1b8b754ec07b3e985db72e857427f49e0
rickkas7 <rickk@rickk.com>
2023-08-24 07:42:51 -0400

    checkpoint

--- scripts/release-update/app.js
+++ scripts/release-update/app.js
@@ -604,6 +604,203 @@ async function fetchVersionDetails(options) {
     return result;
 }
 
+const hexFileEol = '\n';
+
+// Reads a file from disk and returns the contents, possibly with the end-of-line updated
+// to use hexFileEol
+function hexFile(f) {
+    let hex = '';
+
+    fs.readFileSync(f, 'utf8').split('\n').forEach(function(line) {
+        hex += line.trim() + hexFileEol;
+    });
+
+    return hex;
+}
+
+function hexFileText(s) {
+    let hex = '';
+
+    for(let line of s.split('\n')) {
+        hex += line.trim() + hexFileEol;
+    }
+
+    return hex;
+}
+
+// Return the end-of-file indicator. Must be at the end of the hex file.
+function endOfFileHex() {
+    return ':00000001FF' + hexFileEol;
+}
+
+function hexFileWithoutEof(f) {
+    // f is a path to a .hex file
+    const hexData = fs.readFileSync(f, 'utf8');
+    if (hexData.length == 0 || hexData.charAt(0) != ':') {
+        console.log('input file ' + f + ' does not appear to be a hex file');
+        return '';
+    }
+
+    let baseAddr = 0;
+    let hexOut = '';
+
+    hexData.split(/[\r\n]/).some(function(lineData) {
+        lineData = lineData.trim();
+        if (lineData.length == 0) {
+            return false;
+        }
+        if (lineData.charAt(0) != ':') {
+            return false;
+        }
+        //console.log('line: ' + lineData);
+
+        const buf = Buffer.from(lineData.substring(1), 'hex');
+       
+        const len = buf.readUInt8(0);
+        const addr = buf.readUInt16BE(1);
+        const recType = buf.readUInt8(3);
+        // data begins at 4
+        // checksum is last byte
+        const checksum = buf.readUInt8(4 + len);
+
+        const calcChecksum = calculateBufferChecksum(buf);
+
+        if (calcChecksum == checksum) {
+            if (recType == 0x00 && len == 0x00) {
+                // EOF
+                return true;
+            }
+            hexOut += lineData + hexFileEol;
+        }
+        else {
+            console.log('CHECKSUM ERROR! len=' + len + ' addr=' + addr + ' recType=' + recType + ' checksum=' + checksum + ' calcChecksum=' + calcChecksum);
+        }
+        return false;
+    });    
+
+    return hexOut;
+}
+
+function padHex(num, len) {
+    let hex = num.toString(16);
+    if (hex.length < len) {
+        return '00000000'.substring(0, len - hex.length) + hex;
+    }
+    else {
+        return hex;
+    }
+}
+
+function calculateBufferChecksum(buf) {
+    // Last byte of buf is the checksum, so don't include that in
+    // the checksum (buf.length - 1)
+    let sum = 0;
+    for(let ii = 0; ii < (buf.length - 1); ii++) {
+        sum += buf.readUInt8(ii);
+    }
+
+    // Checksum is sum of all bytes, then two's complement
+    // (invert all of the bits and add 1)
+    return (~sum + 1) & 0xff;
+}
+
+// The baseAddr passed to this function is the 16-bit base address, not the actual base address of the file!
+function baseAddrToHex(baseAddr) {
+    // Len (1) + Addr (2) + Type (1) + Data (2) + Checksum (1) = 7
+    let buf = Buffer.alloc(7);
+    buf.writeUInt8(2, 0); // Byte count = 0x02 (offset 0)
+    buf.writeUInt16BE(0, 1); // Address = 0x0000 (offset 1)
+    buf.writeUInt8(4, 3); // Record type = 0x04 (offset 3)
+    buf.writeUInt16BE(baseAddr, 4); // Base addr (offset 4)
+    buf.writeUInt8(calculateBufferChecksum(buf), 6); // Checksum (offset 6)
+    return buf.toString('hex');
+}
+
+async function binFilePathToHex(binFilePath) {
+    return new Promise(function(resolve, reject) {
+        const reader = new HalModuleParser();
+        reader.parseFile(binFilePath, function(fileInfo, err) {
+            if (err) {
+                console.log("error processing file " + binFilePath, err);
+                reject(err);
+            }
+            
+            const loadAddress = parseInt(fileInfo.prefixInfo.moduleStartAddy, 16);
+    
+            // console.log('fileInfo', fileInfo);
+
+            let hex = '';
+
+            if (fileInfo.prefixInfo.moduleFunction == ModuleInfo.FunctionType.RADIO_STACK) {
+                const bufSize = fileInfo.fileBuffer.length - fileInfo.suffixInfo.suffixSize - ModuleInfo.MODULE_PREFIX_SIZE;
+                let buf = Buffer.alloc(bufSize);
+                fileInfo.fileBuffer.copy(buf, 0, ModuleInfo.MODULE_PREFIX_SIZE, fileInfo.fileBuffer.length - fileInfo.suffixInfo.suffixSize);
+                // The load address in the file is 0x1000. I'm not positive this is correct, may need to hardcode 0x0
+                hex = fileBufferToHex(buf, loadAddress);     
+            }
+            else {
+                hex = fileBufferToHex(fileInfo.fileBuffer, loadAddress);       
+            }
+    
+            resolve(hex);    
+        });
+    });
+}
+
+function fileBufferToHex(fileBuffer, loadAddress) {
+    // 
+    let hex = '';
+
+    // loadAddress is the address in the target memory space to load the file
+    // This requires updating bot the baseAddress record (4) and the address.
+    // Both the baseAddress and address in the data record are 16-bit, and
+    // are combined to make a 32-bit address. However, when loading a binary
+    // file it's not always loaded on a 16-bit boundary! 
+    // For example, a Gen 3 user binary is loaded at 0xD4000 (128K) or 
+    // 0xb4000 (256K) with Device OS 3.1 and later.
+    // It does need to be an even multiple of the chunk size, but that's 16
+    // bytes and should not be an issue.
+    
+    // Base address
+    let lastBaseAddr = -1;
+
+    let fileBufferOffset = 0;
+    while(fileBufferOffset < fileBuffer.length) {
+        let baseAddr = loadAddress & 0xffff0000;
+        let curAddr = loadAddress - baseAddr;
+        if (lastBaseAddr != baseAddr) {
+            lastBaseAddr = baseAddr;
+            hex += ':' + baseAddrToHex(baseAddr >> 16) + hexFileEol;
+        }
+
+        let chunkSize = fileBuffer.length - fileBufferOffset;
+        if (chunkSize > 16) {
+            chunkSize = 16;
+        }
+
+        // Len (1) + Addr (2) + Type (1) + Data (chunkSize) + Checksum (1) = 5 + chunkSize
+        let buf = Buffer.alloc(5 + chunkSize);
+
+        buf.writeUInt8(chunkSize, 0); // Byte count = chunkSize (offset 0)
+        buf.writeUInt16BE(curAddr, 1); // Address = 0x0000 (offset 1)
+        buf.writeUInt8(0, 3); // Record type = 0x00 (offset 3)
+
+        fileBuffer.copy(buf, 4, fileBufferOffset,fileBufferOffset + chunkSize);
+
+        buf.writeUInt8(calculateBufferChecksum(buf), 4 + chunkSize); // Checksum (last byte)
+
+        hex += ':' + buf.toString('hex') + hexFileEol;
+
+        fileBufferOffset += chunkSize;
+        loadAddress += chunkSize;
+    }
+
+    
+
+    return hex;
+}
+
+
 async function runDeviceOs() {
     const ignoreVersions = ['3.2.1-p2.1', '3.0.1-p2.4', '3.0.1-p2.5'];
 
@@ -655,6 +852,9 @@ async function runDeviceOs() {
 
             // This is needed so we can tell whether to insert 128K compatibility in hex file
             const isNRF52 = platformInfo.baseMcu.startsWith('nrf52');
+
+            // versionSort sorts descending, so this test looks backwards
+            const add128Kcompatibility = isNRF52 && apiHelper.versionSort(ver.version, '3.1.0') <= 0;
 
             // Should find a better way to determine this. Is used to determine whether to include tracker-edge and monitor-edge firmware
             const isTrackerOrMonitor = (platformId == 26); 
@@ -763,7 +963,7 @@ async function runDeviceOs() {
                         break;
 
                     default:
-                        console.logg('unknown moduleFunction ' + module.prefixInfo.moduleFunction);
+                        console.log('unknown moduleFunction ' + module.prefixInfo.moduleFunction);
                         break;
                 }
                 if (!module.hexToolName) {
@@ -778,18 +978,26 @@ async function runDeviceOs() {
                     const platformUicrFilename = platformInfo.name + '_' + uicrFilename;
 
                     if (hexFiles[platformUicrFilename]) {
-                        hex += hexFiles[platformUicrFilename];
+                        hex += hexFileText(hexFiles[platformUicrFilename]);
                     }
                     else {
-                        hex += hexFiles[uicrFilename];
+                        hex += hexFileText(hexFiles[uicrFilename]);
                     }   
                 }
 
-                if (module.prefixInfo.moduleFunction == 'radio_stack') {
-                    hex += hexFiles['radio_stack_prefix.hex'];
+
+                if (module.prefixInfo.moduleFunction == 'user_part' && add128Kcompatibility) {
+                    // Add 128K binary clearing here if Gen 3 and 3.1.0 or later
+                    let b = Buffer.alloc(1024, 0xff);
+                    hex += fileBufferToHex(b, 0xd4000);
                 }
 
-                // TODO Add the file contents here
+
+                if (module.prefixInfo.moduleFunction == 'radio_stack') {
+                    hex += hexFileText(hexFiles['radio_stack_prefix.hex']);
+                }
+
+                // TODO: On Tracker, swap tracker edge for tinker here!
 
                 // Generate module info JSON
                 await new Promise(function(resolve, reject) {
@@ -809,7 +1017,28 @@ async function runDeviceOs() {
                 });
 
                 // Generate hex
+                hex += await binFilePathToHex(module.binaryFile);
+
+                // Add to zip
+                const content = fs.readFileSync(module.binaryFile);
+                zip.file(module.hexToolName + '.bin', content);
+
             }
+
+
+            // TODO: Add NCP here
+            for(const obj of ncpJson.versions) {
+                // obj.platforms array of platform IDs
+                // .versionMin Minimum semver inclusive
+                // .versionMax Maximum semver exclusive
+                // .file filename (in /assets/files/ncp)
+                if (obj.platforms.includes(platformId)) {
+                    const content = fs.readFileSync(path.join(ncpDir, obj.file));
+                    zip.file(obj.file, content);
+                }
+                
+            }
+
 
             // Generate module info 
             fs.writeFileSync(path.join(outputDir, platformInfo.name + '.json'), JSON.stringify(moduleInfo, null, 2));
@@ -824,17 +1053,12 @@ async function runDeviceOs() {
             });
 
             // Generate hex
+            hex += endOfFileHex();
             fs.writeFileSync(path.join(outputDir, platformInfo.name + '.hex'), hex);
 
 
 
 
-            // Check for NCP 
-            // Not included in hex file. I think it's included in the zip?
-
-            // Check for gen3-128k-compatibility
-            // let b = Buffer.alloc(1024, 0xff);
-            // hex += fileBufferToHex(b, 0xd4000);
 
 
         }
