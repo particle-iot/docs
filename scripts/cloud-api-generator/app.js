77d15851750ee3080d50a015cf64f515889e3322
rickkas7 <rickk@rickk.com>
2023-10-16 10:40:29 -0400

    refactor

--- scripts/cloud-api-generator/app.js
+++ scripts/cloud-api-generator/app.js
@@ -14,136 +14,180 @@ let globalOptions = {
 globalOptions.docsTopDir = path.resolve(path.join(__dirname, '..', '..')),
 
 
-globalOptions.apis = [
-    {
+globalOptions.apis = {
+    cloud: {
         name: 'api-service',
         dirs: [
-            // 'src/views',
+            'src/views',
         ],
         requireApiTag: true,
     },
-    {
+    js: {
         name: 'particle-api-js',
-        dirs: [
-            'src',
+        files: [
+            'src/Particle.js',
         ],
         requireApiTag: false,
     },
-];
+};
 
+async function scanFile(api, curFile) {
+
+    const fileContents = fs.readFileSync(path.join(api.baseDir, curFile), 'utf8');
+
+    const fileLines = fileContents.split('\n');
+
+    let parsed = commentParser.parse(fileContents);
+    
+    for(let ii = 0; ii < parsed.length; ii++) {
+
+        let apiDef = {
+            description: parsed[ii].description,
+            tags: parsed[ii].tags,              
+            // source: parsed[ii].source,
+            functionPrototype: '',
+            line: 1,
+            library: api.name,
+            file: curFile, 
+        };
+
+
+        let isApi = false;
+        let skipApi = false;
+        for(const tag of parsed[ii].tags) {
+            if (tag.tag == 'api') {
+                isApi = true;
+            }
+            if (tag.tag == 'private' || tag.tag == 'apiPrivate') {
+                skipApi = true;
+            }
+            if (tag.tag == 'apiName') {
+                apiDef.apiName = tag.name;
+                console.log('apiName ' + apiDef.apiName);
+            }
+        }
+
+        if (api.requireApiTag && !isApi) {
+            continue;
+        }
+        if (skipApi) {
+            continue;
+        }
+
+        if (parsed[ii].source.length > 0) {
+            apiDef.line = parsed[ii].source[0].number;
+
+            const lastLine = parsed[ii].source[parsed[ii].source.length - 1].number;
+
+            let indent;
+            for(let line = lastLine + 1; line < fileLines.length; line++) {
+                const m = fileLines[line].match(/^([ \t]+)/);
+                if (!m) {
+                    break;
+                }
+
+                if (!indent) {
+                    indent = m[1];
+                }
+                if (m[1].length > indent.length) {
+                    break;
+                }
+
+                apiDef.functionPrototype += fileLines[line].trim() + ' ';
+            }
+
+            apiDef.functionPrototype = apiDef.functionPrototype.replace(/[{ \t]+$/, '');
+
+            if (!apiDef.apiName && apiDef.functionPrototype.length) {
+                const m = apiDef.functionPrototype.match(/([A-Za-z0-9_]+)[ ]*\(/);
+                if (m) {
+                    apiDef.apiName = m[1];
+                }
+                else {
+                    console.log('did not parse functionName ' + apiDef.functionPrototype);
+                }        
+            }
+        }
+
+        // console.log('apiDef', apiDef);
+        api.definitions.push(apiDef);
+
+        if (apiDef.apiName) {
+            api.definitionByName[apiDef.apiName.toLowerCase()] = apiDef;
+        }
+    }                
+}
 
 async function run() {
     // Preflight
-    for(let api of globalOptions.apis) {
-        if (!api.dir) {
-            api.dir = path.resolve(path.join(globalOptions.docsTopDir, '..', api.name));
+    for(const apiName in globalOptions.apis) {
+        let api = globalOptions.apis[apiName];
+
+        if (!api.baseDir) {
+            api.baseDir = path.resolve(path.join(globalOptions.docsTopDir, '..', api.name));
         }
 
-        if (!fs.existsSync(api.dir)) {
-            console.error('missing dir ' + api.dir + ' in ' + api.name);
+        if (!api.definitions) {
+            api.definitions = [];
+        }
+        if (!api.definitionByName) {
+            api.definitionByName = {};
+        }
+
+        if (!fs.existsSync(api.baseDir)) {
+            console.error('missing dir ' + api.baseDir + ' in ' + api.name);
             process.exit(1);
         }
-        
-        for(const dirName of api.dirs) {
-            const curDir = path.join(api.dir, dirName);
-            if (!fs.existsSync(curDir)) {
-                console.error('missing dir ' + curDir + ' in ' + api.name);
-                process.exit(1);
-            }
-            
-            for(const dirEntry of fs.readdirSync(curDir, {withFileTypes: true})) {
-                if (!dirEntry.isFile()) {
-                    continue;
-                }
-                const lastDotIndex = dirEntry.name.lastIndexOf('.');
-                const ext = ((lastDotIndex > 0) ? dirEntry.name.substring(lastDotIndex + 1) : '').toLowerCase();
-                if (!globalOptions.sourceExtensions.includes(ext)) {
-                    continue;
-                }
 
-                const curFile = path.join(curDir, dirEntry.name);
-
-                const fileContents = fs.readFileSync(curFile, 'utf8');
-
-                const fileLines = fileContents.split('\n');
-
-                let parsed = commentParser.parse(fileContents);
-                
-                for(let ii = 0; ii < parsed.length; ii++) {
-
-                    let apiDef = {
-                        description: parsed[ii].description,
-                        tags: parsed[ii].tags,              
-                        // source: parsed[ii].source,
-                        functionPrototype: '',
-                        line: 1,
-                        library: api.name,
-                        dir: dirName,
-                        file: path.join(dirName, dirEntry.name), 
-                    };
-
-
-                    let isApi = false;
-                    let skipApi = false;
-                    for(const tag of parsed[ii].tags) {
-                        if (tag.tag == 'api') {
-                            isApi = true;
-                        }
-                        if (tag.tag == 'private' || tag.tag == 'apiPrivate') {
-                            skipApi = true;
-                        }
-                        if (tag.tag == 'apiName') {
-                            apiDef.apiName = tag.name;
-                        }
-                    }
-
-                    if (api.requireApiTag && !isApi) {
-                        continue;
-                    }
-                    if (skipApi) {
-                        continue;
-                    }
-
-                    if (parsed[ii].source.length > 0) {
-                        apiDef.line = parsed[ii].source[0].number;
-
-                        const lastLine = parsed[ii].source[parsed[ii].source.length - 1].number;
-
-                        let indent;
-                        for(let line = lastLine + 1; line < fileLines.length; line++) {
-                            const m = fileLines[line].match(/^([ \t]+)/);
-                            if (!m) {
-                                break;
-                            }
-
-                            if (!indent) {
-                                indent = m[1];
-                            }
-                            if (m[1].length > indent.length) {
-                                break;
-                            }
-
-                            apiDef.functionPrototype += fileLines[line].trim() + ' ';
-                        }
-
-                        apiDef.functionPrototype = apiDef.functionPrototype.replace(/[{ \t]+$/, '');
-
-                        if (!apiDef.apiName && apiDef.functionPrototype.length) {
-                            const m = apiDef.functionPrototype.match(/([A-Za-z0-9_]+)[ ]*\(/);
-                            if (m) {
-                                apiDef.apiName = m[1];
-                            }
-                            else {
-                                console.log('did not parse functionName ' + apiDef.functionPrototype);
-                            }        
-                        }
-                    }
-
-                    console.log('apiDef', apiDef);
-                }                
+        if (api.files) {
+            for(const file of api.files) {
+                await scanFile(api, file);    
             }
         }
+        else {
+            for(const dirName of api.dirs) {
+                const curDir = path.join(api.baseDir, dirName);
+                if (!fs.existsSync(curDir)) {
+                    console.error('missing dir ' + curDir + ' in ' + api.name);
+                    process.exit(1);
+                }
+                
+                for(const dirEntry of fs.readdirSync(curDir, {withFileTypes: true})) {
+                    if (!dirEntry.isFile()) {
+                        continue;
+                    }
+                    const lastDotIndex = dirEntry.name.lastIndexOf('.');
+                    const ext = ((lastDotIndex > 0) ? dirEntry.name.substring(lastDotIndex + 1) : '').toLowerCase();
+                    if (!globalOptions.sourceExtensions.includes(ext)) {
+                        continue;
+                    }
+    
+                    const curFile = path.join(dirName, dirEntry.name);
+    
+                    await scanFile(api, curFile);
+                }
+            }    
+        }
+        
     }
+
+    // Show cloud APIs
+    
+    for(const apiName in globalOptions.apis.cloud.definitionByName) {
+        //console.log('cloud apiName ' + apiName);
+    }
+    
+
+    // See if Javascript API names can be 
+    
+    for(const apiName in globalOptions.apis.js.definitionByName) {
+        if (!globalOptions.apis.cloud.definitionByName[apiName]) {
+            //console.log('apiName ' + apiName + ' not present in cloud api');
+        }
+        else {
+            console.log('apiName ' + apiName + ' matched cloud api');
+        }
+    }    
 
     // console.log('globalOptions', globalOptions);
 
