511f2cb4393a7dc1ef82ab5d4fe725379d267fcc
rickkas7 <rickk@rickk.com>
2023-10-29 11:26:43 -0400

    checkpoint

--- src/assets/js/collapse.js
+++ src/assets/js/collapse.js
@@ -1,6 +1,9 @@
 
-$(document).ready(function() {
+let imageOverlay = {};
 
+$(document).ready(function() {
+	imageOverlay.setupOverlay();
+	
 	$('input.collapseDefault').each(function(index) {
 		var genericCssClass = $(this).attr('name');
 		var defaultValue = $(this).attr('value');
@@ -188,39 +191,149 @@ function collapseDrag(ev, id) {
     ev.dataTransfer.setData("text", $('#' + id).text());
 }
 
-function showOverlay(imgPath) {
-	var html = '<img src="' + imgPath + '" class="imageOverlay no-darken" />';
-
+imageOverlay.setupOverlay = function() {
+	imageOverlay.backgroundColor = '#000000';
+	imageOverlay.toolsHeight = $('#imageToolsContainer ').height();
 
 	$('.imageOverlayCloseIcon').on('click', function() {
-		hideOverlay();
+		imageOverlay.hideOverlay();
+	});
+	$('.imageOverlayMinusIcon').on('click', function() {
+		imageOverlay.zoomOut();
+	});
+	$('.imageOverlayPlusIcon').on('click', function() {
+		imageOverlay.zoomIn();
 	});
 
 	$('#imageOverlayContainer').on('click', function() {
-		hideOverlay();
+		imageOverlay.hideOverlay();
 	});
+
+	// TODO: Handle resize event
+}
+
+imageOverlay.zoomIn = function() {
+	console.log('imageOverlay.zoomIn');
+}
+
+imageOverlay.zoomOut = function() {
+	console.log('imageOverlay.zoomOut');
+}
+
+imageOverlay.draw = function() {
+	const ctx = $(imageOverlay.canvas)[0].getContext("2d");
+    ctx.fillStyle = imageOverlay.backgroundColor;
+    ctx.fillRect(0, imageOverlay.toolsHeight, imageOverlay.canvasWidth, imageOverlay.canvasHeight);
+
+	let info = {
+		scale: 1,
+		dx: 0,
+		dy: imageOverlay.toolsHeight,
+	};
+
+	// scaleX, scaleY are 
+	// == 1 if the image and canvas as the same size, 
+	// < 1 if the image is larger than the canvas
+	// > 1 if the canvas is larger than the image
+	info.scaleX = imageOverlay.canvasWidth / imageOverlay.imageWidth;
+	info.scaleY = (imageOverlay.canvasHeight - imageOverlay.toolsHeight) / imageOverlay.imageHeight;
+
+	if (info.scaleX < 1 && info.scaleY < 1) {
+		// Image is larger than the canvas in both dimensions (scale down)
+		// The smallest value indicates the maximum scaling so the image will fit entirely, possibly with empty space
+		info.scale = Math.min(info.scaleX, info.scaleY);
+	}
+	else
+	if (info.scaleX > 1 && info.scaleY > 1) {
+		// Canvas is larger than the image in both dimensions (scale up)
+		// The largest value indicates the minimum scaling so the image will fit entirely, possibly with empty space
+		info.scale = Math.max(info.scaleX, info.scaleY);
+	}
+	else
+	if (info.scaleX < 1 && info.scaleY >= 1) {
+		// Image is larger than the canvas in the X dimension
+		// Canvas is larger than the image in the Y dimension
+		info.scale = info.scaleX;
+	}
+	else
+	if (info.scaleX >= 1 && info.scaleY < 1) {
+		// Image is larger than the canvas in the X dimension
+		// Canvas is larger than the image in the Y dimension
+		info.scale = info.scaleY;
+	}
+	else {
+		// Exactly the same size (unlikely to occur)
+		// (info.scale stays at 1)
+	}
+
+	info.dWidth = imageOverlay.imageWidth * info.scale;
+	info.dHeight = imageOverlay.imageHeight * info.scale;
+
+	if (info.dWidth < imageOverlay.canvasWidth) {
+		info.dx = Math.floor((imageOverlay.canvasWidth - info.dWidth) / 2);
+	}
+	if (info.dHeight < imageOverlay.canvasHeight) {
+		info.dy = Math.floor(imageOverlay.toolsHeight + (imageOverlay.canvasHeight - info.dHeight) / 2);
+	}
+
+	console.log('info', info);
+
+	// drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
+	ctx.drawImage(imageOverlay.image, 0, 0, imageOverlay.imageWidth, imageOverlay.imageHeight, info.dx, info.dy, info.dWidth, info.dHeight);
+}
+
+imageOverlay.resize = function() {
+	imageOverlay.canvasWidth = Math.floor($('#imageOverlayContainer').width());
+	imageOverlay.canvasHeight = Math.floor($('#imageOverlayContainer').height());
+
+	// $(imageOverlay.canvas).width(imageOverlay.canvasWidth);
+	// $(imageOverlay.canvas).height(imageOverlay.canvasHeight);
+
+	$(imageOverlay.canvas).prop('width', imageOverlay.canvasWidth);
+	$(imageOverlay.canvas).prop('height', imageOverlay.canvasHeight);
+
+	imageOverlay.draw();	 
+}
+
+imageOverlay.showOverlay = function(imgPath) {
+	$('#imageOverlayContainer').show();
+	$('#imageToolsContainer').show();
+
+	imageOverlay.canvas = $('#imageOverlay > canvas');
+
+	imageOverlay.image = new Image();
+	imageOverlay.image.onload = function() {
+		imageOverlay.imageWidth = imageOverlay.image.width;
+		imageOverlay.imageHeight = imageOverlay.image.height;	
+		console.log('loaded', imageOverlay);
+		imageOverlay.resize();
+	}
+	imageOverlay.image.src = imgPath;
+	
+	imageOverlay.zoom = 1;
 
 	$('body').keydown(function(ev) {
 		switch (ev.key) {
             case 'Esc':
             case 'Escape':
-				hideOverlay();
+				imageOverlay.hideOverlay();
+				ev.preventDefault();
 				break;
+
+			// Keyboard shortcuts for zoom in (Cmd-+ on the Mac, for example, are intercepted by the browser)
 
 			default:
 				break;
 		}
 	});
 
-	$('#imageOverlay').html(html);
-	$('#imageOverlayContainer').show();
 }
 
-function hideOverlay() {
-	$('.imageOverlayCloseIcon').off('click');
-	$('#imageOverlayContainer').off('click');
+imageOverlay.hideOverlay = function() {
+	imageOverlay.image = null;
 	$('body').off('keydown');
 	$('#imageOverlayContainer').hide();
+	$('#imageToolsContainer').hide();
 }
 
 
