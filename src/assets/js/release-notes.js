f406fb7c5d9860a70797c09779a4d3a85cabf2c4
rickkas7 <rickk@rickk.com>
2024-04-12 04:53:44 -0400

    checkpoint
    - not functional

--- src/assets/js/release-notes.js
+++ src/assets/js/release-notes.js
@@ -11,6 +11,85 @@ $(document).ready(function() {
     const defaultSearchAfter = 'v1.4.4';
     const gaCategory = 'Release Note Search';
 
+    let releaseNotePlatforms = [];
+    let allTags = [];
+
+    // Platforms to hide from the filter select
+    const hidePlatforms = ['trackerm'];
+
+    // Mapped tags (lowercased)
+    const mapTags = {
+        'm som': 'msom',
+    };
+    const ignoreTags = [
+        'ci',
+        'test',
+    ];
+
+    const includePlatform = function(entry, filterPlatform) {
+        if (!filterPlatform) { 
+            // No filtering enabled
+            return true;
+        }
+
+        if (entry.tags.length == 0) {
+            // No tags, include (just in case)
+            return true;
+        }
+
+        let sanitizedTags = [];
+        for(const tag of entry.tags) {
+            if (mapTags[tag]) {
+                sanitizedTags.push(mapTags[tag]);
+                continue;
+            }
+            
+            const slashParts = tag.split('/');
+            if (slashParts.length > 1) {
+                for(let tag2 of slashParts) {
+                    tag2 = tag2.trim();
+                    sanitizedTags.push(tag2);
+                }
+                continue;
+            }
+
+            sanitizedTags.push(tag);
+        }
+
+        for(const tag of sanitizedTags) {
+            if (ignoreTags.includes(tag)) {
+                return false;
+            }
+        }        
+        
+        {
+            const generations = [];
+            for(const tag of sanitizedTags) {
+                const m = tag.match(/gen[ ]*([0-9]+)/);
+                if (m) {
+                    generations.push(parseInt(m[1]));
+                }
+            }
+            
+            if (generations.length) {
+                // If there is any gen set, any one can match, otherwise filter out
+                for(const gen of generations) {
+                    if (gen == filterPlatform.generation) {
+                        console.log('filter generations allow', generations);
+                        return true;
+                    }
+                }
+                console.log('filter generations omit', generations);
+                return false;
+            }
+        }
+
+
+//        console.log('sanitizedTags', sanitizedTags);
+        
+        return true;
+    }
+
     const doSetup = function() {
         $('.apiHelperReleaseNotes').each(function() {
             const thisPartial = $(this);
@@ -64,7 +143,26 @@ $(document).ready(function() {
                     textWidth -= 80;
                 }
 
+                let filterPlatform;
+                if ($(thisPartial).find('.filterDevice').prop('checked')) {
+                    const platformId = parseInt($(thisPartial).find('.filterPlatform').val());
+
+                    filterPlatform = releaseNotePlatforms.find(e => e.id == platformId);
+                }
+
                 for(const entry of items) {
+                    for(let tag of entry.tags) {
+                        tag = tag.toLowerCase();
+                        if (!allTags.includes(tag)) {
+                            allTags.push(tag);
+                        }
+                    }
+
+                    if (!includePlatform(entry, filterPlatform)) {
+                        console.log('includePlatform exclude', entry);
+                        continue;
+                    }
+
                     const trElem = document.createElement('tr');
 
                     {
@@ -129,7 +227,17 @@ $(document).ready(function() {
             const renderList = function(sectionData, options = {}) {
                 const outputElem = $(thisPartial).find('.apiHelperOutput');
 
+                let filterPlatform;
+                if ($(thisPartial).find('.filterDevice').prop('checked')) {
+                    const platformId = parseInt($(thisPartial).find('.filterPlatform').val());
+                    filterPlatform = releaseNotePlatforms.find(e => e.id == platformId);
+                }
+
                 for(const section in sectionData) {
+                    if (filterPlatform && section == 'INTERNAL') {
+                        continue;
+                    }
+
                     const sectionElem = document.createElement('h3');
                     $(sectionElem).text(section);
                     $(outputElem).append(sectionElem);
@@ -161,6 +269,12 @@ $(document).ready(function() {
 
                 let searchParams = new URLSearchParams();
                 searchParams.set('mode', mode);
+
+                if ($(thisPartial).find('.filterDevice').prop('checked')) {
+                    const platformId = parseInt($(thisPartial).find('.filterPlatform').val());
+                    searchParams.set('filter', platformId);
+                }
+
 
                 if (mode == 'rel1') {
                     const ver = $(thisPartial).find('.versionSelect').val();
@@ -290,6 +404,8 @@ $(document).ready(function() {
                         renderOneList(items, {showVersion:true});    
                     }
                 }
+                console.log('allTags', allTags);
+
                 window.history.pushState({}, '', '?' + searchParams.toString());
             };
         
@@ -354,6 +470,13 @@ $(document).ready(function() {
                 renderPage();
             });
 
+            $(thisPartial).find('.filterDevice').on('click', function() {
+                renderPage();
+            });
+            $(thisPartial).find('.filterPlatform').on('change', function() {
+                renderPage();
+            });
+
                 
             $(thisPartial).find('input:radio[name=mode][value=rel1]').on('change', renderPage);
 
@@ -405,6 +528,12 @@ $(document).ready(function() {
 
 
             if (urlParams) {
+                const filterPlatform = urlParams.get('filter');
+                if (filterPlatform) {
+                    $(thisPartial).find('.filterPlatform').val(filterPlatform);
+                    $(thisPartial).find('.filterDevice').prop('checked', true);
+                }
+
                 const mode = urlParams.get('mode');
                 if (mode == 'rel1') {
                     const ver = urlParams.get('ver');
@@ -453,6 +582,31 @@ $(document).ready(function() {
         });
 
     };
+
+    apiHelper.getCarriersJson().then(function(carriersJson) {
+        // console.log(carriersJson);
+        // carriersJson.deviceConstants (object)
+        //   baseMcu, features, generation, name, public, productEligible, displayName
+        for(const key in carriersJson.deviceConstants) {
+            if (carriersJson.deviceConstants[key].public && carriersJson.deviceConstants[key].productEligible) {
+                if (!hidePlatforms.includes(carriersJson.deviceConstants[key].name)) {
+                    releaseNotePlatforms.push(Object.assign({}, carriersJson.deviceConstants[key]));
+                }
+            }
+        }
+
+        // Sort by display name, alphabetical
+        releaseNotePlatforms.sort(function(a, b) {
+            return a.displayName.localeCompare(b.displayName);
+        });
+
+        for(const p of releaseNotePlatforms) {
+            const optionElem = document.createElement('option');
+            $(optionElem).attr('value', p.id.toString());
+            $(optionElem).text(p.displayName);
+            $('.filterPlatform').append(optionElem);
+        }
+    });
     
     fetch('/assets/files/releaseNotes.json')
         .then(response => response.json())
