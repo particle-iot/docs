94491b828a0ff3ea9bde4070f7a3260c9f8e3c55
Julien Vanier <julien@particle.io>
2024-05-15 23:06:12 -0400

    Update API docs

--- scripts/apidoc.js
+++ scripts/apidoc.js
@@ -7,158 +7,9 @@ var path = require('path');
 
 var apiScopes = [];
 
-function trimParam(string) {
-  if (!string) {
-    return string;
-  }
-  var pCount = (string.match(/<p>/g) || []).length;
-  if (pCount !== 1) {
-    return string;
-  }
-  if (string.indexOf('<p>') !== 0 || string.indexOf('</p>') !== string.length - 4) {
-    return string;
-  }
-  return string.trim().slice(3, -4);
-}
-
-function trimParameters(allParams) {
-  if (!allParams) return;
-
-  _.each(allParams, function (params, key) {
-    params.forEach(function (param) {
-      // remove unnecessary <p></p> tags from start and end
-      param.type = trimParam(param.type);
-      param.description = param.description;
-    });
-  });
-}
-
-function nestParameters(allParams) {
-  if (allParams) {
-    _.each(allParams, function (params, key) {
-      var indexedParameters = _.keyBy(allParams[key], 'field');
-      var nestedParams = [];
-      params.forEach(function (param) {
-        var nameParts = param.field.split('.');
-        if (nameParts.length > 1) {
-          var parent = indexedParameters[nameParts[0]];
-          if (parent) {
-            if (!parent.nestedParams) {
-              parent.nestedParams = [];
-            }
-            param.field = nameParts.slice(1).join('.');
-            parent.nestedParams.push(param);
-            nestedParams.push(param);
-          }
-        }
-      });
-      allParams[key] = _.difference(allParams[key], nestedParams);
-    });
-  }
-}
-
-function breakOutAlternativeOperations(data) {
-  for (var i=0; i < data.length; i++) {
-    var route = data[i];
-    if (!route.parameter) continue;
-
-    var baseParams, baseSuccess, baseError;
-    var examplesIndex = {};
-    var successExamplesIndex = {};
-    var errorExamplesIndex = {};
-    if (route.examples) {
-      examplesIndex = _.groupBy(route.examples, 'title');
-    } else {
-      route.examples = {};
-    }
-
-    if (route.success) {
-      successExamplesIndex = _.groupBy(route.success.examples, 'title');
-    } else {
-      route.success = {};
-    }
-
-    if (route.error) {
-      errorExamplesIndex = _.groupBy(route.error.examples, 'title');
-    } else {
-      route.error = {};
-    }
-
-    _.each(route.parameter.fields, function(params, title) {
-      if (title === 'Parameter') {
-        baseParams = params;
-        baseSuccess = (route.success.fields || {})['Success 200'] || [];
-        baseError = (route.error.fields || {})['Error 4xx'] || [];
-        return;
-      }
-
-      // create new clone route
-      var groupKey = params[0].group;
-      var newRoute = _.cloneDeep(route);
-      newRoute.title = title;
-      newRoute.description = null;
-      newRoute.parameter.fields = {
-        Parameter: baseParams.concat(params)
-      };
-
-      // grab op specific response fields too
-      var responseFields = (route.success.fields || {})[title];
-      if (responseFields) {
-        newRoute.success.fields = {
-          'Success 200': baseSuccess.concat(responseFields)
-        };
-        delete route.success.fields[title];
-      }
-      var errorFields = (route.error.fields || {})[title];
-      if (errorFields) {
-        newRoute.error.fields = {
-          'Error 4xx': baseError.concat(errorFields)
-        };
-        delete route.error.fields[title];
-      }
-
-      // copy over specific examples
-      if (examplesIndex[groupKey] && examplesIndex[groupKey].length) {
-        newRoute.examples = examplesIndex[groupKey];
-        newRoute.examples.forEach(function (ex) {
-          ex.title = ex.content;
-        });
-      } else {
-        newRoute.examples = null;
-      }
-      // remove copied examples from base
-      route.examples = _.difference(route.examples, newRoute.examples);
-
-      if (successExamplesIndex[groupKey] && successExamplesIndex[groupKey].length) {
-        newRoute.success.examples = successExamplesIndex[groupKey];
-        newRoute.success.examples.forEach(function (ex) {
-          ex.title = ex.content;
-        });
-      } else {
-        newRoute.success.examples = null;
-      }
-      route.success.examples = _.difference(route.success.examples, newRoute.success.examples);
-
-      if (errorExamplesIndex[groupKey] && errorExamplesIndex[groupKey].length) {
-        newRoute.error.examples = errorExamplesIndex[groupKey];
-        newRoute.error.examples.forEach(function (ex) {
-          ex.title = ex.content;
-        });
-      } else {
-        newRoute.error.examples = null;
-      }
-      route.error.examples = _.difference(route.error.examples, newRoute.error.examples);
-
-      // add to list of all routes
-      i++;
-      data.splice(i, 0, newRoute);
-    });
-  }
-}
-
 function assignOrder(data) {
   data.forEach(function (route) {
-    var groupParts = route.group.split('_');
+    var groupParts = route.group.split('.');
     route.group = groupParts[0];
     route.order = +groupParts[1] || 99;
   });
@@ -170,11 +21,12 @@ module.exports = function(options) {
       const savePath = path.join(__dirname, '..', 'generated', path.basename(apiOptions.src) + '.json');
       apiOptions.parse = true;
       apiOptions.src = metalsmith.path(apiOptions.src);
+      apiOptions.config = metalsmith.path(apiOptions.config);
       if (!fs.existsSync(apiOptions.src)) {
         if (fs.existsSync(savePath)) {
           return JSON.parse(fs.readFileSync(savePath, 'utf8'));
         }
-        
+
         return null;
       }
       var dirFiles = fs.readdirSync(apiOptions.src);
@@ -182,7 +34,11 @@ module.exports = function(options) {
         return null;
       }
 
-      var apiReturn = apidoc.createDoc(apiOptions);
+      const options = {
+        ...apiOptions,
+        src: [apiOptions.src]
+      };
+      var apiReturn = apidoc.createDoc(options);
       if (!apiReturn) {
         return new Error('Error');
       }
@@ -219,7 +75,6 @@ module.exports = function(options) {
     }
 
     assignOrder(apiData);
-    breakOutAlternativeOperations(apiData);
 
     apiData.forEach(function (route) {
       if (route.permission) {
@@ -229,20 +84,6 @@ module.exports = function(options) {
             apiScopes.push(obj.name);
           }
         }
-      }
-
-      if (route.parameter) {
-        trimParameters(route.parameter.fields);
-      }
-
-      if (route.success) {
-        trimParameters(route.success.fields);
-        nestParameters(route.success.fields);
-      }
-
-      if (route.error) {
-        trimParameters(route.error.fields);
-        nestParameters(route.error.fields);
       }
     });
 
