eeb2aa679d9ac074ee7ad30e0ae57bb3be3ce8b5
rickkas7 <rickk@rickk.com>
2024-07-29 09:06:05 -0400

    checkpoint
    - not functional

--- src/assets/js/ble-tdm.js
+++ src/assets/js/ble-tdm.js
@@ -7,8 +7,6 @@ $(document).ready(function() {
 
         calc.urlParams = new URLSearchParams(window.location.search);
 
-        calc.sensorDivTemplateElem = $(thisPartial).find('.sensorDiv')[0].cloneNode(true);
-        console.log('sensorDivTemplateElem', calc.sensorDivTemplateElem );
 
         calc.inputValues = {};
         calc.inputUrlParams = {};
@@ -52,6 +50,8 @@ $(document).ready(function() {
             $(thisPartial).find('.sensorDiv').each(function() {
                 const sensorDivElem = $(this);
 
+                const sensorIndex = parseInt($(sensorDivElem).data('sensor'));
+
                 let sensorObj = {};
 
                 $(sensorDivElem).find('.sensorInputParam').each(function() {
@@ -64,12 +64,12 @@ $(document).ready(function() {
                     if (key) {
                         sensorObj[key.key] = $(inputElem).val();                    
     
-                        calc.inputUrlParams[key.urlParam + calc.inputValues.sensors.length] = sensorObj[key.key];
+                        calc.inputUrlParams[key.urlParam + sensorIndex] = sensorObj[key.key];
                     }
     
                 });
 
-                calc.inputValues.sensors.push(sensorObj);
+                calc.inputValues.sensors[sensorIndex] = sensorObj;
             });
 
         };
@@ -108,16 +108,30 @@ $(document).ready(function() {
             }
         };
 
+        calc.sensorKeys = [];
+        calc.sensorDivTemplateElem = $(thisPartial).find('.sensorDiv')[0].cloneNode(true);
+        $(thisPartial).find('.sensorInputParam').each(function() {
+            const inputElem = $(this);
+
+            let obj = {};
+            obj.inputType = $(inputElem).prop('type');
+            obj.key = calc.parseKey($(inputElem).data('key'));
+            if (obj.key) {
+                calc.sensorKeys.push(obj);
+            }
+        });
+
         $(thisPartial).find('.inputParam,.sensorInputElem').each(calc.inputHandler);
 
-        $(thisPartial).find('.addSensor').on('click', function() {
+        calc.addSensor = function() {
             const sensorElem = calc.sensorDivTemplateElem.cloneNode(true);
             $(sensorElem).data('sensor', calc.inputValues.sensors.length);
             $(sensorElem).find('h3').text('Sensor ' + (calc.inputValues.sensors.length + 1));
             $(sensorElem).find('.sensorInputElem').each(calc.inputHandler);
             $(thisPartial).find('.sensorsDiv').append(sensorElem);
-        });
+        }
 
+        $(thisPartial).find('.addSensor').on('click', calc.addSensor);
 
         if (calc.urlParams) {
             $(thisPartial).find('.inputParam').each(function() {
@@ -139,10 +153,34 @@ $(document).ready(function() {
             });
 
             calc.inputValues.sensors = [];
+
+            for(const keyObj of calc.sensorKeys) {
+                for(let sensorIndex = 0; true; sensorIndex++) {
+                    const value = calc.urlParams.get(keyObj.key.urlParam + sensorIndex);
+                    if (!value) {
+                        break;
+                    }
+
+                    let sensorDivElem;
+
+                    for(let tries = 0; tries < 10; tries++) {
+                        sensorDivElem = $(thisPartial).find('sensorDiv[data-index=' + sensorIndex + ']');
+                        if (sensorDivElem.length) {
+                            break;
+                        }
+                        calc.addSensor();
+                    }
+
+                }
+            }
+
+            /*
             $(thisPartial).find('.sensorDiv').each(function() {
                 const sensorDivElem = $(this);
 
                 let sensorObj = {};
+                const sensorIndex = parseInt($(sensorDivElem).data('sensor'));
+
 
                 $(sensorDivElem).find('.sensorInputParam').each(function() {
                     const inputElem = $(this);
@@ -163,6 +201,7 @@ $(document).ready(function() {
 
                 calc.inputValues.sensors.push(sensorObj);
             });
+            */
         }    
 
         console.log('loaded ble-tdm');
